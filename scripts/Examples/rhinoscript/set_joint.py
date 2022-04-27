38740df130cff438165d9f3ffe524ebb7a98bd73
funkchaser <apolinarska@arch.ethz.ch>
2022-04-27 15:21:20 +0200

    setting joint types in rhinodoc with sticky

---
+++ scripts/Examples/rhinoscript/set_joint.py
@@ -0,0 +1,111 @@
+import Rhino
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
+
+
+
+from compas_timber.utils.compas_extra import intersection_line_line_3D
+
+#sc.sticky['manual_joints']=[ ] #list of [centrelineA_guid, centrelineB_guid, joint_type]
+
+
+tol = 1e-6
+
+def detect_connection_topology(centrelineA_guid, centrelineB_guid, max_distance=0.0, tol=tol):
+    
+    centrelineA = rs.coerceline(centrelineA_guid)
+    centrelineB = rs.coerceline(centrelineB_guid)
+    
+    def __is_near_end(t, tol=tol):
+        if abs(t)<tol: return True #almost zero
+        if abs(1.0-t)<tol: return True #almost 1
+        return False
+    
+    result = ()
+
+    tA,tB = intersection_line_line_3D(centrelineA, centrelineB, max_distance, True, True, tol)
+            
+    if tA==None or tB==None: pass
+    else:
+        xA=__is_near_end(tA)
+        xB=__is_near_end(tB)
+        if all([xA,xB]):
+            #L-joint (meeting at ends)
+            result = ('L',[centrelineA_guid, centrelineB_guid],[tA, tB])
+        elif not any([xA,xB]):
+            #X-joint (meeting somewhere along the line)
+            result = ('X',[centrelineA_guid, centrelineB_guid],[tA, tB])
+        else:
+            #T-joint (one meeting with the end along the other)
+            if xA:
+                #j:main, i:cross
+                result = ('T',[centrelineA_guid, centrelineB_guid],[tA, tB])
+            if xB:
+                #j:main, i:cross
+                result = ('T',[centrelineB_guid, centrelineA_guid],[tB, tA])
+    return result
+
+
+def cmd_string_options(message="Choose:", oplist=["Option1", "Option2"], default_index = 0):
+    """
+    message: [str] prompt to the user
+    oplist: [str] list of options to display
+    default_index : [int] index of the element in the oplist which should be the default option
+    
+    returns: [str] the selected or default option, None on fail
+    """
+    
+    gs = Rhino.Input.Custom.GetOption()
+    gs.SetCommandPrompt(message)
+    for op in oplist:
+        gs.AddOption(op)
+    gs.SetDefaultString(oplist[default_index])
+    gs.AcceptNothing(True)
+    
+    while True:
+        result = gs.Get()
+        if result == Rhino.Input.GetResult.Cancel: 
+            return None
+        if gs.GotDefault(): 
+            return "default"
+        else:
+            return gs.Option().EnglishName
+
+
+def set_T_butt_joint(mainbeam, crossbeam):
+    sc.sticky['manual_joints'].append([mainbeam, crossbeam, 'T-Butt'])
+
+def set_T_lap_joint(mainbeam, otherbeam):
+    sc.sticky['manual_joints'].append([mainbeam, crossbeam, 'T-Lap'])
+
+def set_T_lap_joint(mainbeam, otherbeam):
+    sc.sticky['manual_joints'].append([mainbeam, crossbeam, 'T-MortiseTenon'])
+
+if __name__ == "__main__":
+
+    # object filters https://developer.rhino3d.com/api/rhinoscript/selection_methods/getobject.htm
+
+    LineA_guid = rs.GetObject("Select the centreline of the first beam", 4, False, False)
+    LineB_guid = rs.GetObject("Select the centreline of the second beam", 4, False, False)
+
+    rs.SelectObjects([LineA_guid,LineB_guid])
+    
+    max_distance = rs.GetReal("Maximum allowed distance between lines..",1e-3, 0, None)
+    if not max_distance: max_distance = tol
+    joint_topo, [mainbeam, otherbeam], [tA,tB] = detect_connection_topology(LineA_guid,LineB_guid,max_distance)
+    print "This is a %s type of joint"%joint_topo
+
+    if joint_topo == 'T':
+        joint_type = cmd_string_options("Select joint type:..", ["Butt", "Lap", "Mortise-Tenon"], 0)
+        if joint_type == 'Butt':          set_T_butt_joint(mainbeam, otherbeam)
+        if joint_type == 'Lap':           set_T_lap_joint(mainbeam, otherbeam)
+        if joint_type == 'Mortise-Tenon': set_T_mortisetenon_joint(mainbeam, otherbeam)
+    elif joint_topo == 'L':
+        joint_type = cmd_string_options("Select joint type:..", ["Butt", "Lap", "Miter", "Bridle"], 0)
+    elif joint_topo == 'X':
+        joint_type = cmd_string_options("Select joint type:..", ["Lap", "Smth else"], 0)
+    elif joint_topo == 'I':
+        joint_type = None
+
+    print("The %s-%s joint was set."%(joint_topo,joint_type))
+
--- src/compas_timber/connections/t_butt.py
+++ src/compas_timber/connections/t_butt.py
@@ -11,10 +11,16 @@ class TButtJoint(Data):
         self.main_beam_key = main_beam.key
         self.cross_beam_key = cross_beam.key
         # self.gap = gap #float, additional gap, e.g. for glue
+        self.joint_type_name = 'T-Butt'
+        self.frame = None # will be needed as coordinate system for structural calculations for the forces at the joint
 
         assembly.add_joint(self)
         assembly.connect(self,[main_beam, cross_beam])
 
+    # TODO: add this property to some joint template class
+    @property
+    def beams(self):
+        return [self.main_beam, self.cross_beam]
 
     @property
     def main_beam(self): 
