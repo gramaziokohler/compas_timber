f58d1cdf7e736b49804bc99df7a66a235e70ce40
funkchaser <apolinarska@arch.ethz.ch>
2022-05-20 12:13:22 +0200

    Revert "make compas_timber installable with compas_rhino.install"
    This reverts commit 01ac57222a2dfd70d3a634f19cd9b07bcfb56739.

--- src/compas_timber/rhino/install.py
+++
@@ -1,6 +0,0 @@
-import compas.plugins
-
-@compas.plugins.plugin(category='install')
-def installable_rhino_packages():
-    return ['compas_timber'] #add all other dependencies that are needed and work in rhino
-    
