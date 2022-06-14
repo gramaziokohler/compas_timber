4eb3f39dbdfaeec785285d746b2553d71ffa8796
funkchaser <apolinarska@arch.ethz.ch>
2022-06-14 14:04:35 +0200

    speed plots

--- tests/performance.py
+++ tests/performance.py
@@ -6,6 +6,7 @@ from compas.geometry import Frame, Point, Vector
 from compas_timber.assembly.assembly import TimberAssembly
 from compas_timber.parts.beam import Beam
 from compas_timber.connections.joint import Joint
+from compas_timber.connections.t_butt import TButtJoint
 from compas_timber.utils.workflow import set_defaul_joints
 
 import matplotlib.pyplot as plt
@@ -27,7 +28,7 @@ def timeit(method):
 
 @timeit
 def large_assembly(size=100):
-
+    t0 = time.time()
     n = size-2
     w = 0.1
     h = 0.16
@@ -43,31 +44,43 @@ def large_assembly(size=100):
     # make sleepers
     beams.append(Beam.from_endpoints(Point(0, 0, 0), Point(d*n, 0, 0), width=w, height=h))
     beams.append(Beam.from_endpoints(Point(0, y, 0), Point(d*n, y, 0), width=w, height=h))
+    t1 = time.time()
 
     A = TimberAssembly()
     for b in beams:
         A.add_beam(b)
+    print(A.find(beams[0]))
 
-    #set_defaul_joints(A) #-> ca 9s for 500 beams
+    t2 = time.time()
+    # set_defaul_joints(A) #-> ca 9s for 500 beams
+    for i in range(n):
+        TButtJoint(beams[i], beams[n], A)
+        TButtJoint(beams[i], beams[n+1], A)
+
+    t3 = time.time()
+    return [t0, t1, t2, t3]
 
 
 if __name__ == "__main__":
-    
-    model_sizes=[]
-    exec_times =[]
 
-    for i in range(20):
+    model_sizes = []
+    exec_times = []
+
+    for i in range(1):
         s = i*100
-        ts = time.time()
-        large_assembly(s)
-        te = time.time()
-        t = (te-ts)*1000
+        #ts = time.time()
+        t = large_assembly(s)
+        #te = time.time()
+        #t = (te-ts)*1000
 
         model_sizes.append(s)
         exec_times.append(t)
 
     print(exec_times)
-    plt.plot(model_sizes, exec_times)
+    plt.plot(model_sizes, [(t1-t0)*1000 for t0, t1, t2, t3 in exec_times], 'b', label='create beams')
+    plt.plot(model_sizes, [(t2-t1)*1000 for t0, t1, t2, t3 in exec_times], 'g', label='add beams to assembly')
+    plt.plot(model_sizes, [(t3-t2)*1000 for t0, t1, t2, t3 in exec_times], 'r', label='create joints explicitly')
     plt.xlabel('no of beams')
     plt.ylabel('[ms]')
+    plt.legend(loc="upper left")
     plt.show()
