c43a7fa382c0345a14e6fa00826fc1d7ce677c1a
Chen Kasirer <kasirer@arch.ethz.ch>
2022-06-27 11:35:15 +0200

    added second level import for main modules

--- src/compas_timber/assembly/__init__.py
+++ src/compas_timber/assembly/__init__.py
@@ -1,3 +1,6 @@
 """
 This will be the place for tools to plan the fabrication process, e.g. define gripping planes, insertion paths, assembly sequence etc.
 """
+from .assembly import TimberAssembly
+
+__all__ = [_ for _ in dir() if not _.startswith("_")]
