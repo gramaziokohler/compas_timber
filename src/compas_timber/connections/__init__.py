a6a4cd56cd061d7b68bc770b490c5433aaab1b5b
funkchaser <apolinarska@arch.ethz.ch>
2022-08-04 12:03:23 +0200

    new: l-miter joint

--- src/compas_timber/connections/__init__.py
+++ src/compas_timber/connections/__init__.py
@@ -3,5 +3,6 @@ Connections are a collection of tools to generate joint geometries.
 """
 from .joint import Joint
 from .t_butt import TButtJoint
+from .l_miter import LMiterJoint
 
 __all__ = [_ for _ in dir() if not _.startswith("_")]
---
+++ src/compas_timber/connections/l_miter.py
@@ -0,0 +1,90 @@
+from compas.geometry import Plane
+from compas.geometry import Point
+from compas.geometry import Vector
+from compas.geometry import cross_vectors
+
+from compas_timber.utils.compas_extra import intersection_line_line_3D
+
+from ..connections.joint import Joint
+
+
+class LMiterJoint(Joint):
+    def __init__(self, assembly, beamA, beamB):
+
+        super(LMiterJoint, self).__init__(assembly, [beamA, beamB])
+        self.beamA = beamA
+        self.beamB = beamB
+        self.beamA_key = None
+        self.beamB_key = None
+        self.features = []
+
+        
+    @property
+    def joint_type(self):
+        return "L-Miter"
+
+
+    @property
+    def data(self):
+        data_dict = {
+            "beamA_key": self.beamA.key,
+            "beamB_key": self.beamB.key,
+        }
+        data_dict.update(Joint.data.fget(self))
+        return data_dict
+
+    @data.setter
+    def data(self, value):
+        Joint.data.fset(self, value)
+        self.beamA_key = value["beamA_key"]
+        self.beamB_key = value["beamB_key"]
+
+
+    def add_feature(self):
+        """
+        Adds the feature definitions (geometry, operation) to the involved beams.
+        In a T-Butt joint, adds the trimming plane to the main beam (no features for the cross beam).
+        """
+        # TODO: how to saveguard this being added multiple times?
+        plnA, plnB = self.cutting_planes
+        self.beamA.add_feature(plnA, "trim")
+        self.beamB.add_feature(plnB, "trim")
+        # pass
+
+    @property
+    def cutting_planes(self):
+
+        vA = Vector(*self.beamA.frame.xaxis)  # frame.axis gives a reference, not a copy
+        vB = Vector(*self.beamB.frame.xaxis)
+
+        # intersection point (average) of both centrelines
+        [pxA,tA], [pxB,tB] = intersection_line_line_3D(
+            self.beamA.centreline,
+            self.beamB.centreline,
+            max_distance=self.beamA.height + self.beamB.height,
+            limit_to_segments=False,
+            tol=self.assembly.tol,
+        )
+
+        p = Point((pxA.x + pxB.x) * 0.5, (pxA.y + pxB.y) * 0.5, (pxA.z + pxB.z) * 0.5)
+
+        # makes sure they point outward of a joint point
+        tA, _ = self.beamA.endpoint_closest_to_point(pxA)
+        if tA == "end":
+            vA *= -1.0
+        tB, _ = self.beamB.endpoint_closest_to_point(pxB)
+        if tB == "end":
+            vB *= -1.0
+
+        # bisector
+        v_bisector = vA + vB
+        v_bisector.unitize()
+
+        # get frame
+        v_perp = Vector(*cross_vectors(v_bisector, vA))
+        v_normal = Vector(*cross_vectors(v_bisector, v_perp))
+
+        plnA = Plane(p, v_normal * -1.0)
+        plnB = Plane(p, v_normal)
+
+        return [plnA, plnB]
