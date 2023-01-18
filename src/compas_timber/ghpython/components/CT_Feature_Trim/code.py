217b65f61d1ba1a98a8cf23e144bde2c606a18f9
Chen Kasirer <ckasirer@ethz.ch>
2023-01-18 16:45:47 +0100

    beam trimming component

--- src/compas_timber/ghpython/components/CT_Feature_Trim/code.py
+++ src/compas_timber/ghpython/components/CT_Feature_Trim/code.py
@@ -1,14 +1,30 @@
+from ghpythonlib.componentbase import executingcomponent as component
+from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
 
-from compas_timber.utils.workflow import FeatureDefinition
-import Grasshopper.Kernel as ghk
+from compas_rhino.conversions import plane_to_compas_frame
 
-warning = ghk.GH_RuntimeMessageLevel.Warning
-error = ghk.GH_RuntimeMessageLevel.Error
+from compas_timber.parts import BeamTrimmingFeature
+from compas_timber.ghpython import FeatureDefinition
 
-if not Beam:
-    ghenv.Component.AddRuntimeMessage(warning, "Input parameter Beam failed to collect data")
-if not Pln:
-    ghenv.Component.AddRuntimeMessage(warning, "Input parameter Pln failed to collect data")
-    
-if Beam and Pln:
-    Ft = [FeatureDefinition('trim', p, b) for b in Beam for p in Pln]
+class TrimmingFeature(component):
+
+    def RunScript(self, beams, planes):
+
+        if not beams:
+            self.AddRuntimeMessage(Warning, "Input parameter Beam failed to collect data")
+        if not planes:
+            self.AddRuntimeMessage(Warning, "Input parameter Pln failed to collect data")
+        if not (beams and planes):
+            return
+
+        if not isinstance(beams, list):
+            beams = [beams]
+        if not isinstance(planes, list):
+            planes = [planes]
+
+        feautre_defs = []
+        for plane in planes:
+            feature = BeamTrimmingFeature(plane_to_compas_frame(plane))
+            feautre_defs.append(FeatureDefinition(feature, beams))
+
+        return feautre_defs
