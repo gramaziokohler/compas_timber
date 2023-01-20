4d44d167edada869e9aa6ee09040907ba89711ad
Chen Kasirer <ckasirer@ethz.ch>
2023-01-20 13:58:07 +0100

    clearing Nones from component input lists

--- src/compas_timber/ghpython/components/CT_Assembly/code.py
+++ src/compas_timber/ghpython/components/CT_Assembly/code.py
@@ -9,15 +9,18 @@ class Assembly(component):
     def RunScript(self, beams, joints, features):
         assembly = TimberAssembly()
         if beams:
+            beams = [b for b in beams if b is not None]
             for beam in beams:
                 beam.clear_features()  # since we're editing the beams
                 assembly.add_beam(beam)
 
         if joints:
+            joints = [j for j in joints if j is not None]
             for joint in joints:
                 joint.joint_type.create(assembly, *joint.beams)
 
         if features:
+            features = [f for f in features if f is not None]
             for f_def in features:
                 for beam in f_def.beams:
                     print(beam.features)
