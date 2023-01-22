f4037f63e51e6ae45e010889cb0c9f73af84d8a7
Chen Kasirer <ckasirer@ethz.ch>
2023-01-22 16:51:00 +0100

    reworked LButt joint

--- src/compas_timber/connections/l_butt.py
+++ src/compas_timber/connections/l_butt.py
@@ -1,16 +1,25 @@
-from compas.geometry import close
 from compas.geometry import Frame
 
-from compas_timber.connections import Joint
-from compas_timber.connections import beam_side_incidence
+from compas_timber.parts import BeamExtensionFeature
+from compas_timber.parts import BeamTrimmingFeature
+
+from .joint import Joint
+from .joint import beam_side_incidence
+from .solver import JointTopology
 
 
 class LButtJoint(Joint):
-    def __init__(self, assembly, main_beam, cross_beam):
+
+    SUPPORTED_TOPOLOGY = JointTopology.L
+
+    def __init__(self, assembly=None, main_beam=None, cross_beam=None):
         super(LButtJoint, self).__init__(assembly, [main_beam, cross_beam])
         self.main_beam_key = main_beam.key
         self.cross_beam_key = cross_beam.key
+        self.main_beam = main_beam
+        self.cross_beam = cross_beam
         self.gap = 0.0  # float, additional gap, e.g. for glue
+        self.features = []
 
     @property
     def data(self):
@@ -29,27 +38,17 @@ class LButtJoint(Joint):
         self.cross_beam_key = value["cross_beam_key"]
         self.gap = value["gap"]
 
-    def __eq__(self, other):
-        tol = self.assembly.tol
-        return (
-            isinstance(other, LButtJoint)
-            and super(LButtJoint, self).__eq__(other)
-            and self.main_beam_key == other.main_beam_key
-            and self.cross_beam_key == other.cross_beam_key
-            and close(self.gap, other.gap, tol)
-        )
+    @property
+    def beams(self):
+        return [self.main_beam, self.cross_beam]
+
+    def restore_beams_from_keys(self, assemly):
+        self.main_beam = assemly.find_by_key(self.main_beam_key)
+        self.cross_beam = assemly.find_by_key(self.cross_beam_key)
 
     @property
     def joint_type(self):
         return "L-Butt"
-
-    @property
-    def main_beam(self):
-        return self.assembly.find_by_key(self.main_beam_key)
-
-    @property
-    def cross_beam(self):
-        return self.assembly.find_by_key(self.cross_beam_key)
 
     @property
     def cutting_plane_main(self):
@@ -57,30 +56,29 @@ class LButtJoint(Joint):
         cfr = min(angles_faces, key = lambda x: x[0])[1]
         cfr = Frame(cfr.point, cfr.xaxis, cfr.yaxis*-1.0) #flip normal
         return cfr
-    
+
     @property
     def cutting_plane_cross(self):
         angles_faces = beam_side_incidence(self.cross_beam, self.main_beam)
         cfr = max(angles_faces, key = lambda x: x[0])[1]
         return cfr
 
-
     def add_features(self):
         """
         Adds the feature definitions (geometry, operation) to the involved beams.
-        In a L-Butt joint, adds the trimming plane to the main beam (no features for the cross beam).
+        In a T-Butt joint, adds the trimming plane to the main beam (no features for the cross beam).
+
         """
-        cfr_main = self.cutting_plane_main
-        cfr_cross = self.cutting_plane_cross
+        if self.features:
+            self.main_beam.clear_features(self.features)
+            self.cross_beam.clear_features(self.features)
+            self.features = []
 
-        self.main_beam.add_feature(self.main_beam.extension_to_plane(cfr_main), "extend")
-        self.main_beam.add_feature(cfr_main, "trim")        
-        self.cross_beam.add_feature(self.cross_beam.extension_to_plane(cfr_cross), "extend")
-        #self.cross_beam.add_feature(cfr_cross, "trim")
+        main_extend = BeamExtensionFeature(*self.main_beam.extension_to_plane(self.cutting_plane_main))
+        main_trim = BeamTrimmingFeature(self.cutting_plane_main)
+        cross_extend = BeamExtensionFeature(*self.cross_beam.extension_to_plane(self.cutting_plane_cross))
 
-
-
-
-if __name__ == "__main__":
-
-    pass
+        self.main_beam.add_feature(main_extend)
+        self.main_beam.add_feature(main_trim)
+        self.cross_beam.add_feature(cross_extend)
+        self.features.extend([main_extend, main_trim, cross_extend])
