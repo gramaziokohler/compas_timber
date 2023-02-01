9259ca9285834ab7190109e2c737d441a190b3ad
apolinarska@arch.ethz.ch <apolinarska@arch.ethz.ch>
2023-02-01 17:25:42 +0100

    GH components: NEW - BeamDecompose

---
+++ src/compas_timber/ghpython/components/CT_BeamDecompose/code.py
@@ -0,0 +1,32 @@
+"""Extracts main geometric characteristics of a beam."""
+from compas.artists import Artist
+from compas_rhino.conversions import box_to_rhino
+from compas_rhino.conversions import frame_to_rhino
+from compas_rhino.conversions import line_to_rhino_curve
+from ghpythonlib.componentbase import executingcomponent as component
+from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
+
+from compas_timber.utils.ghpython import list_input_valid
+
+
+class MyComponent(component):
+    def RunScript(self, Beam):
+        if not Beam:
+            self.AddRuntimeMessage(Warning, "Input parameter Beam failed to collect data")
+
+        Frame = []
+        Centerline = []
+        Box = []
+        Brep = []
+        Width = []
+        Height = []
+        if list_input_valid(ghenv, Beam, "Beam"):
+            Frame = [frame_to_rhino(b.frame) for b in Beam]
+            Centerline = [line_to_rhino_curve(b.centerline) for b in Beam]
+            Box = [box_to_rhino(b.shape) for b in Beam]
+            #Brep = [Artist(b.get_geometry(True)).draw() for b in Beam]
+            Brep = [b.get_geometry().native_brep for b in Beam]
+            Width = [b.width for b in Beam]
+            Height = [b.height for b in Beam]
+
+        return (Frame, Centerline, Box, Brep, Width, Height)
