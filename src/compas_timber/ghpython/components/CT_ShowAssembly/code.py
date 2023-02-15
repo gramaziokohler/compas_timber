00ba1ffece2259a68ecac9237236b8857e86c5dd
apolinarska@arch.ethz.ch <apolinarska@arch.ethz.ch>
2023-02-15 22:59:36 +0100

    GH: apply_features toggle moved from ShowAssembly to Assembly

--- src/compas_timber/ghpython/components/CT_ShowAssembly/code.py
+++ src/compas_timber/ghpython/components/CT_ShowAssembly/code.py
@@ -4,7 +4,7 @@ from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
 
 
 class ShowAssembly(component):
-    def RunScript(self, Assembly, showFeatures):
+    def RunScript(self, Assembly):
         if not Assembly:
             self.AddRuntimeMessage(Warning, "Input parameter Assembly failed to collect data")
             return
@@ -13,9 +13,6 @@ class ShowAssembly(component):
         Geometry = []
         Errors = []
         for beam in Assembly.beams:
-            if showFeatures:
-                Errors.extend(beam.apply_features())
-            brep = beam.get_geometry(showFeatures)
-            Geometry.append(Artist(brep).draw())
+            Geometry.append(Artist(beam.get_geometry(True)).draw())
 
-        return Geometry, Errors
+        return Geometry
