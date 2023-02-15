d3749af78bb202965de2cde7d1ae2617e58df69c
Chen Kasirer <ckasirer@ethz.ch>
2023-02-15 09:39:31 +0100

    some more case fixes changes

--- src/compas_timber/ghpython/components/CT_FindByIndex/code.py
+++ src/compas_timber/ghpython/components/CT_FindByIndex/code.py
@@ -3,20 +3,20 @@ from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
 
 
 class FindBeamByBeamIndex(component):
-    def RunScript(self, Beams, Indices):
+    def RunScript(self, Beams, indices):
         if not Beams:
             self.AddRuntimeMessage(Warning, "Input parameter Beams failed to collect data")
-        if not Indices:
-            self.AddRuntimeMessage(Warning, "Input parameter Indices failed to collect data")
-        if not (Beams and Indices):
+        if not indices:
+            self.AddRuntimeMessage(Warning, "Input parameter indices failed to collect data")
+        if not (Beams and indices):
             return
 
-        if not isinstance(Indices, list):
-            Indices = [Indices]
-        Indices = [int(i) for i in Indices]
+        if not isinstance(indices, list):
+            indices = [indices]
+        indices = [int(i) for i in indices]
 
         FoundBeams = []
-        for index in Indices:
+        for index in indices:
             try:
                 FoundBeams.append(Beams[index])
             except IndexError:
