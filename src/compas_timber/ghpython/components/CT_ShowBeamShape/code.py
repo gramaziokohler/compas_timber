fd5056df2782d3d2dd94f3172c2f439142c50240
apolinarska@arch.ethz.ch <apolinarska@arch.ethz.ch>
2023-02-15 22:27:51 +0100

    minor misc fixes

--- src/compas_timber/ghpython/components/CT_ShowBeamShape/code.py
+++ src/compas_timber/ghpython/components/CT_ShowBeamShape/code.py
@@ -3,5 +3,5 @@ from compas_rhino.conversions import box_to_rhino
 
 from compas_timber.ghpython.ghcomponent_helpers import list_input_valid
 
-if list_input_valid(ghenv, Beam, "Beam"):
+if list_input_valid(ghenv.Component, Beam, "Beam"):
     Brep = [box_to_rhino(b.shape).ToBrep() for b in Beam]
