5fc945927217c16e2b252871e49ce2528badd1cc
apolinarska@arch.ethz.ch <apolinarska@arch.ethz.ch>
2023-02-28 15:16:43 +0100

    Revert "GH: revised extracting/showing Beam geometry"
    This reverts commit 5e7a47190a334c254753089e028f8a0af232a7fa.

--- src/compas_timber/ghpython/components/CT_ShowAssembly/code.py
+++ src/compas_timber/ghpython/components/CT_ShowAssembly/code.py
@@ -1,3 +1,4 @@
+from compas.artists import Artist
 from ghpythonlib.componentbase import executingcomponent as component
 from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
 
@@ -7,5 +8,10 @@ class ShowAssembly(component):
         if not Assembly:
             self.AddRuntimeMessage(Warning, "Input parameter Assembly failed to collect data")
             return
+        Assembly = Assembly.copy()  # we're gonna be making changes to upstream objects
 
-        return Assembly.beams
+        Geometry = []
+        for beam in Assembly.beams:
+            Geometry.append(Artist(beam.get_geometry(True)).draw())
+
+        return Geometry
