aa27c9b08a69683203a17530112177a9dc42f235
Chen Kasirer <ckasirer@ethz.ch>
2023-04-19 17:15:25 +0200

    more cleanup, more docstrings

--- src/compas_timber/parts/beam.py
+++ src/compas_timber/parts/beam.py
@@ -62,6 +62,8 @@ class Beam(Part):
         Width of the cross-section
     height : float
         Height of the cross-section
+    geometry_type : str
+        The type of geometry created for this beam. Either 'mesh' or 'brep.
 
     Attributes
     ----------
@@ -109,9 +111,8 @@ class Beam(Part):
 
     def __init__(self, frame, length, width, height, geometry_type, **kwargs):
         super(Beam, self).__init__(frame=frame)
-        self.frame = (
-            frame  # TODO: add setter so that only that makes sure the frame is orthonormal --> needed for comparisons
-        )
+        # TODO: add setter so that only that makes sure the frame is orthonormal --> needed for comparisons
+        self.frame = frame
         self.width = width
         self.height = height
         self.length = length
@@ -235,11 +236,23 @@ class Beam(Part):
         )
 
     def update_beam_geometry(self):
+        """Resets the geometry representation of the beam accroding to the current parametric values.
+
+        Should be called after each update to the paramteric definition of the beam.
+
+        """
         self._geometry_with_features = self._create_beam_shape_from_params(
             self.length, self.width, self.height, self.geometry_type
         )
 
     def is_identical(self, other):
+        """Returns True if the other beam's values are identicale, within TOLERANCE, to the ones of this beam.
+
+        Returns
+        -------
+        bool
+
+        """
         return (
             isinstance(other, Beam)
             and close(self.width, other.width, DEFAULT_TOLERANCE)
@@ -251,11 +264,29 @@ class Beam(Part):
 
     @classmethod
     def from_data(cls, data):
+        """Alternative to None default __init__ parameters.
+
+        """
         obj = cls(**data)
         obj.data = data
         return obj
 
     def get_geometry(self, with_features=False):
+        """Returns the geometry representation of this beam.
+
+        The geometry is transformed to the frame of this beam.
+
+        Parameters
+        ----------
+        with_features : bool
+            If True the geometry returned should include the features, if any.
+
+        Returns
+        -------
+        :class:`compas.geometry.Geometry`
+
+
+        """
         transformation = Transformation.from_frame(self.frame)
         if not with_features or not self.features:
             g_copy = self._geometry.copy()
@@ -265,14 +296,30 @@ class Beam(Part):
         return g_copy
 
     def add_feature(self, feature, apply=False):
+        """Adds a feature to this beam.
+
+        If apply is False, `apply_features` must be called for the features to be represented in the geometry.
+
+        feature : :class:`~compas.datastructures.Feature`
+            The feature to be added to this beam.
+        apply : bool
+            If True, the feature will be applied to the beam's geometry upon adding it.
+
+        """
         self.features.append(feature)
         if apply:
             self.apply_features()
 
     def apply_features(self):
-        """
-        Iterate over features:
-            if is_cumulative:
+        """Applies the features which were added to this beam.
+
+        This method separatelly applies the parametric and geometric features.
+        The parametric features, if any, are accumulated when possible.
+
+        Returns
+        -------
+        list(str)
+            A list of errors which occurred during the application of the features, if any, to assist with debugging.
 
         """
         error_log = []
@@ -295,6 +342,17 @@ class Beam(Part):
         return msg.format(feature, getattr(feature, "_owner", None), part)
 
     def clear_features(self, features_to_clear=None):
+        """Clears applied features and restores their effect.
+
+        Selective removal of features is possible by providing a list of features which shall be removed.
+        In such case, all features are restored and the remaining features are reapplied.
+
+        Parameters
+        ----------
+        features_to_clear : list(:class:`compas.datastructures.Feature`)
+            If provided, only the features which are in this list shall be removed.
+
+        """
         if features_to_clear:
             self.features = [f for f in self.features if f not in features_to_clear]
         else:
@@ -303,9 +361,28 @@ class Beam(Part):
 
     @classmethod
     def from_centerline(cls, centerline, width, height, z_vector=None, geometry_type="brep"):
-        """
-        Define the beam from its centerline.
-        z_vector: a vector indicating the height direction (z-axis) of the cross-section. If not specified, a default will be used.
+        """Define the beam from its centerline.
+
+        Parameters
+        ----------
+        centerline : :class:`~compas.geometry.Line`
+            The centerline of the beam to be created.
+        length : float
+            Length of the beam.
+        width : float
+            Width of the cross-section.
+        height : float
+            Height of the cross-section.
+        z_vector : :class:`~compas.geometry.Vector`
+            A vector indicating the height direction (z-axis) of the cross-section.
+            Defaults to WorldZ or WorldX depending on the centerline's orientation.
+        gemetry_type : str
+            The type of geometry to use when creating this beam. Either 'mesh' of 'brep'.
+
+        Returns
+        -------
+        :class:`~compas_timber.parts.Beam`
+
         """
         x_vector = centerline.vector
         z_vector = z_vector or cls._calculate_z_vector_from_centerline(x_vector)
@@ -319,15 +396,35 @@ class Beam(Part):
 
     @classmethod
     def from_endpoints(cls, point_start, point_end, width, height, z_vector=None, geometry_type="brep"):
-        line = Line(point_start, point_end)
+        """Creates a Beam from the given endpoints.
 
+        Parameters
+        ----------
+        point_start : :class:`~compas.geometry.Point`
+            The start point of a centerline
+        end_point : :class:`~compas.geometry.Point`
+            The end point of a centerline
+        width : float
+            Width of the cross-section.
+        height : float
+            Height of the cross-section.
+        z_vector : :class:`~compas.geometry.Vector`
+            A vector indicating the height direction (z-axis) of the cross-section.
+            Defaults to WorldZ or WorldX depending on the centerline's orientation.
+        gemetry_type : str
+            The type of geometry to use when creating this beam. Either 'mesh' of 'brep'.
+
+        Returns
+        -------
+        :class:`~compas_timber.parts.Beam`
+
+        """
+        line = Line(point_start, point_end)
         return cls.from_centerline(line, width, height, z_vector, geometry_type)
 
     def move_endpoint(self, vector=Vector(0, 0, 0), which_endpoint="start"):
-        # TODO: revise if needed, compare to ParametricFeature
-        # create & apply a transformation
-        """
-        which_endpoint: 'start' or 'end' or 'both'
+        """Deprecated?
+
         """
         z = self.frame.zaxis
         ps = self.centerline_start
@@ -341,8 +438,6 @@ class Beam(Part):
         frame = Frame(ps, x, y)
         self.frame = frame
         self.length = distance_point_point(ps, pe)
-
-        return
 
     def extension_to_plane(self, pln):
         """Returns the amount by which to extend the beam in each direction using metric units.
@@ -380,26 +475,41 @@ class Beam(Part):
         return -ds, de
 
     def extend_ends(self, d_start, d_end):
-        # TODO: revise if needed, compare to ParametricFeature
-        """
+        """Extends the beam's parametric definition at both ends by the given values.
+
         Extensions at the start of the centerline should have a negative value.
         Extenshions at the end of the centerline should have a positive value.
         Otherwise the centerline will be shortend, not extended.
+
+        The geometry of the beam is subsequently updated to match the new values.
+
+        Parameters
+        ----------
+        d_start : float
+            The amount by which the start of the beam's centerline should be extended, in design units.
+        d_end : float
+            The amount by which the end of the beam's centerline should be extended, in design units.
+
         """
         self.frame.point += -self.frame.xaxis * d_start  # "extension" to the start edge
         extension = d_start + d_end
         self.length += extension
         self.update_beam_geometry()
 
-    def rotate_around_centerline(self, angle, clockwise=False):
-        # create & apply a transformation
-        pass
-
     def align_z(self, vector):
+        """Align the z_axis of the beam's definition with the given vector.
+
+        TODO: Not used anywhere. Needed?
+
+        Parameters
+        ----------
+        vector : :class:`~compas.geometry.Vector`
+            The vector with which to align the z_axis.
+
+        """
         y_vector = Vector(*cross_vectors(self.frame.xaxis, vector)) * -1.0
         frame = Frame(self.frame.point, self.frame.xaxis, y_vector)
         self.frame = frame
-        return
 
     @staticmethod
     def _calculate_z_vector_from_centerline(centerline_vector):
@@ -410,6 +520,20 @@ class Beam(Part):
         return z
 
     def endpoint_closest_to_point(self, point):
+        """Returns which endpoint of the centerline of the beam is closer to the given point.
+
+        Parameters
+        ----------
+        point : :class:`~compas.geometry.Point`
+            The point of interest.
+
+        Returns
+        -------
+        list(str, :class:`~compas.geometry.Point`)
+            Two element list. First element is either 'start' or 'end' depending on the result.
+            The second element is the actual endpoint of the beam's centerline which correspond to the result.
+
+        """
         ps = self.centerline_start
         pe = self.centerline_end
         ds = point.distance_to_point(ps)
@@ -419,7 +543,3 @@ class Beam(Part):
             return ["start", ps]
         else:
             return ["end", pe]
-
-
-if __name__ == "__main__":
-    pass
