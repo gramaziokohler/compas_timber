585d721f287c87b55124daab231e7d4e1276f88b
Chen Kasirer <ckasirer@ethz.ch>
2023-04-26 16:40:15 +0200

    linting and formatting

--- src/compas_timber/ghpython/workflow.py
+++ src/compas_timber/ghpython/workflow.py
@@ -150,7 +150,7 @@ class Attribute:
 
 
 def guess_joint_topology_2beams(beamA, beamB, tol=1e-6, max_distance=1e-6):
-    #TODO: replace default max_distance ~ zero with global project precision
+    # TODO: replace default max_distance ~ zero with global project precision
 
     [pa, ta], [pb, tb] = intersection_line_line_3D(beamA.centerline, beamB.centerline, max_distance, True, tol)
 
