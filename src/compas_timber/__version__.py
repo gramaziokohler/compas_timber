a639d0e9f03fd9275ebd6cc757b2cc62dfb6370e
Chen Kasirer <ckasirer@ethz.ch>
2023-05-11 19:31:03 +0200

    updated bumpversion.cfg

--- src/compas_timber/__version__.py
+++ src/compas_timber/__version__.py
@@ -1,11 +1,11 @@
-__title__ = "compas_timber_structures"
+__title__ = "compas_timber"
 __description__ = "COMPAS package for modeling, designing and fabricating timber assemblies."
-__url__ = "https://github.com/gramaziokohler/compas_timber_structures"
+__url__ = "https://github.com/gramaziokohler/compas_timber"
 __version__ = "0.1.0"
 __author__ = "Gramazio Kohler Research"
 __author_email__ = "gramaziokohler@arch.ethz.ch"
 __license__ = "MIT license"
-__copyright__ = "Copyright 2021 Gramazio Kohler Research"
+__copyright__ = "Copyright 2023 Gramazio Kohler Research"
 
 __all__ = [
     "__author__",
