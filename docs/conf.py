6f260b2db70a1e6707e30b2cd0a47761aac637c5
Chen Kasirer <ckasirer@ethz.ch>
2023-05-11 19:32:04 +0200

    Bump version to 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "COMPAS TIMBER"
 year = "2021"
 author = "Gramazio Kohler Research"
 copyright = "{0}, {1}".format(year, author)
-release = "0.1.0"
+release = "0.2.0"
 version = ".".join(release.split(".")[0:2])
 
 master_doc = "index"
