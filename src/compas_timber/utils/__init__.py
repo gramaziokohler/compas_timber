cf5603237f3df743036d2b9084286083a66d1719
Chen Kasirer <ckasirer@ethz.ch>
2023-05-11 14:01:14 +0200

    removed module utils from api docs

--- src/compas_timber/utils/__init__.py
+++ src/compas_timber/utils/__init__.py
@@ -1,24 +1,3 @@
-"""
-********************************************************************************
-utils
-********************************************************************************
-
-.. currentmodule:: compas_timber.utils
-
-.. rst-class:: lead
-
-Functions
-=======
-
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
-    intersection_line_line_3D
-    intersection_line_plane
-
-"""
-
 from .compas_extra import intersection_line_line_3D
 from .compas_extra import intersection_line_plane
 
