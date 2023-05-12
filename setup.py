cb0c1a5aa47ee40d67c5b96fc191b8296960d40b
Chen Kasirer <ckasirer@ethz.ch>
2023-05-12 20:03:50 +0200

    replaced hardcoded information in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# flake8: noqa
 from __future__ import absolute_import
 from __future__ import print_function
 
@@ -16,22 +15,24 @@ here = path.abspath(path.dirname(__file__))
 def read(*names, **kwargs):
     return io.open(path.join(here, *names), encoding=kwargs.get("encoding", "utf8")).read()
 
+about = {}
+exec(read("src", "compas_timber", "__version__.py"), about)
 
 long_description = read("README.md")
 requirements = read("requirements.txt").split("\n")
 optional_requirements = {}
 
 setup(
-    name="compas_timber",
-    version="0.1.0",
-    description="COMPAS package for modeling, designing and fabricating timber assemblies",
+    name=about["__title__"],
+    version=about["__version__"],
+    license=about["__license__"],
+    description=about["__description__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
     long_description=long_description,
-    url="https://github.com/gramaziokohler/compas_timber",
-    author="Gramazio Kohler Research",
-    author_email="gramaziokohler@arch.ethz.ch",
-    license="MIT license",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3- Alpha",
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: MIT License",
@@ -40,10 +41,12 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: IronPython",
     ],
     keywords=[],
     project_urls={},
@@ -54,7 +57,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=requirements,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     extras_require=optional_requirements,
     entry_points={
         "console_scripts": [],
