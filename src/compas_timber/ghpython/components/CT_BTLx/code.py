7d4189d77e380a035a25fb3b4b6df29e30c01ce4
obucklin <obucklin@gmail.com>
2023-08-24 10:57:25 +0200

    fix file path

--- src/compas_timber/ghpython/components/CT_BTLx/code.py
+++ src/compas_timber/ghpython/components/CT_BTLx/code.py
@@ -3,17 +3,14 @@ from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
 from compas_timber.utils.btlx import BTLx
 
 class WriteBTLx(component):
-    def RunScript(self, Assembly, Location, Name, Write):
+    def RunScript(self, Assembly, Path,  Write):
         if not Assembly:
             self.AddRuntimeMessage(Warning, "Input parameter Assembly failed to collect data")
             return
-        if not Location:
-            self.AddRuntimeMessage(Warning, "Input parameter Location failed to collect data")
-            return
-        if not Name:
-            self.AddRuntimeMessage(Warning, "Input parameter Name failed to collect data")
+        if not Path:
+            self.AddRuntimeMessage(Warning, "Input parameter Path failed to collect data")
             return
 
         btlx = BTLx(Assembly)
         if Write:
-            btlx.writeBTLx(Location, Name)
+            btlx.writeBTLx(Path)
