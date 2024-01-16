f95cbc73cd3462698c8a87d631468747e695245a
Chen Kasirer <ckasirer@ethz.ch>
2024-01-16 18:38:28 +0100

    restored casually deleted tasks.py

---
+++ tasks.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import os
+
+from compas_invocations import build
+from compas_invocations import docs
+from compas_invocations import style
+from compas_invocations import tests
+from invoke import Collection
+
+ns = Collection(
+    docs.help,
+    style.check,
+    style.lint,
+    style.format,
+    docs.docs,
+    docs.linkcheck,
+    tests.test,
+    tests.testdocs,
+    tests.testcodeblocks,
+    build.prepare_changelog,
+    build.clean,
+    build.release,
+    build.build_ghuser_components,
+)
+ns.configure(
+    {
+        "base_folder": os.path.dirname(__file__),
+        "ghuser": {
+            "source_dir": "src/compas_timber/ghpython/components",
+            "target_dir": "src/compas_timber/ghpython/components/ghuser",
+            "prefix": "CT: ",
+        },
+    }
+)
