37f916c4c8c14a927a1ba4ac31798631101214fe
obucklin <obucklin@gmail.com>
2024-01-22 18:40:36 +0100

    fixed components

--- src/compas_timber/ghpython/components/CT_Joint_Options_LButt/code.py
+++ src/compas_timber/ghpython/components/CT_Joint_Options_LButt/code.py
@@ -6,8 +6,8 @@ from compas_timber.ghpython import JointOptions
 
 
 class LButtJointOptions(component):
-    def RunScript(self, Gap, SmallBeamButts):
-        args = {"gap": Gap, "small_beam_butts": SmallBeamButts}
+    def RunScript(self, gap, small_beam_butts):
+        args = {"gap": gap, "small_beam_butts": small_beam_butts}
         options = JointOptions(LButtJoint, **args)
 
         return options
