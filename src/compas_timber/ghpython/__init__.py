2b02b08bcd02ff79b56572204d19963239524e9c
Chen Kasirer <ckasirer@ethz.ch>
2024-01-28 15:28:33 +0100

    added container for debug information to workflow.py

--- src/compas_timber/ghpython/__init__.py
+++ src/compas_timber/ghpython/__init__.py
@@ -4,5 +4,14 @@ from .workflow import DirectRule
 from .workflow import FeatureDefinition
 from .workflow import JointOptions
 from .workflow import JointDefinition
+from .workflow import DebugInfomation
 
-__all__ = ["JointDefinition", "CategoryRule", "TopologyRule", "DirectRule", "FeatureDefinition", "JointOptions"]
+__all__ = [
+    "JointDefinition",
+    "CategoryRule",
+    "TopologyRule",
+    "DirectRule",
+    "FeatureDefinition",
+    "JointOptions",
+    "DebugInfomation"
+]
