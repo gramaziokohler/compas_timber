1c640c3336981c01dee13e2909576ab09f51af22
Chen Kasirer <ckasirer@ethz.ch>
2024-02-02 17:34:48 +0100

    blank line

--- src/compas_timber/rhino/install.py
+++ src/compas_timber/rhino/install.py
@@ -4,4 +4,3 @@ import compas.plugins
 @compas.plugins.plugin(category="install")
 def installable_rhino_packages():
     return ["compas_timber"]  # add all other dependencies that are needed and work in rhino
-
