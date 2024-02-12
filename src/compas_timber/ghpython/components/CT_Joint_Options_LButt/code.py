9dc66a77501e638deb743496f3bcd8e3a5206a84
Chen Kasirer <ckasirer@ethz.ch>
2024-02-12 13:26:14 +0100

    updated component

--- src/compas_timber/ghpython/components/CT_Joint_Options_LButt/code.py
+++ src/compas_timber/ghpython/components/CT_Joint_Options_LButt/code.py
@@ -6,12 +6,12 @@ from compas_timber.ghpython import JointOptions
 
 
 class LButtJointOptions(component):
-    def RunScript(self, small_beam_butts, extend_cross):
+    def RunScript(self, small_beam_butts, modify_cross):
         args = {}
         if small_beam_butts is not None:
             args["small_beam_butts"] = small_beam_butts
-        if extend_cross is not None:
-            args["extend_cross"] = extend_cross
+        if modify_cross is not None:
+            args["modify_cross"] = modify_cross
 
         options = JointOptions(LButtJoint, **args)
 
