fdb95058e09708ef6befaaa80097df1ca1504aec
obucklin <obucklin@gmail.com>
2024-03-07 20:12:47 +0100

    beam name chenged

--- src/compas_timber/connections/l_halflap.py
+++ src/compas_timber/connections/l_halflap.py
@@ -17,9 +17,9 @@ class LHalfLapJoint(LapJoint):
 
     Parameters
     ----------
-    main_beam : :class:`~compas_timber.parts.Beam`
+    beam_a : :class:`~compas_timber.parts.Beam`
         The main beam to be joined.
-    cross_beam : :class:`~compas_timber.parts.Beam`
+    beam_b : :class:`~compas_timber.parts.Beam`
         The cross beam to be joined.
     flip_lap_side : bool
         If True, the lap is flipped to the other side of the beams.
@@ -30,13 +30,13 @@ class LHalfLapJoint(LapJoint):
     ----------
     beams : list(:class:`~compas_timber.parts.Beam`)
         The beams joined by this joint.
-    main_beam : :class:`~compas_timber.parts.Beam`
+    beam_a : :class:`~compas_timber.parts.Beam`
         The main beam to be joined.
-    cross_beam : :class:`~compas_timber.parts.Beam`
+    beam_b : :class:`~compas_timber.parts.Beam`
         The cross beam to be joined.
-    main_beam_key : str
+    beam_a_key : str
         The key of the main beam.
-    cross_beam_key : str
+    beam_b_key : str
         The key of the cross beam.
     features : list(:class:`~compas_timber.parts.Feature`)
         The features created by this joint.
@@ -48,40 +48,40 @@ class LHalfLapJoint(LapJoint):
 
     SUPPORTED_TOPOLOGY = JointTopology.TOPO_L
 
-    def __init__(self, main_beam=None, cross_beam=None, flip_lap_side=False, cut_plane_bias=0.5, frame=None, key=None):
-        super(LHalfLapJoint, self).__init__(main_beam, cross_beam, flip_lap_side, cut_plane_bias, frame, key)
+    def __init__(self, beam_a=None, beam_b=None, flip_lap_side=False, cut_plane_bias=0.5, frame=None, key=None):
+        super(LHalfLapJoint, self).__init__(beam_a, beam_b, flip_lap_side, cut_plane_bias, frame, key)
 
     @property
     def joint_type(self):
         return "L-HalfLap"
 
     def add_features(self):
-        assert self.main_beam and self.cross_beam
+        assert self.beam_a and self.beam_b
 
         try:
             main_cutting_frame = self.get_main_cutting_frame()
             cross_cutting_frame = self.get_cross_cutting_frame()
-            negative_brep_main_beam, negative_brep_cross_beam = self._create_negative_volumes()
+            negative_brep_beam_a, negative_brep_beam_b = self._create_negative_volumes()
         except Exception as ex:
             raise BeamJoinningError(beams=self.beams, joint=self, debug_info=str(ex))
 
-        start_main, end_main = self.main_beam.extension_to_plane(main_cutting_frame)
-        start_cross, end_cross = self.cross_beam.extension_to_plane(cross_cutting_frame)
+        start_main, end_main = self.beam_a.extension_to_plane(main_cutting_frame)
+        start_cross, end_cross = self.beam_b.extension_to_plane(cross_cutting_frame)
 
         extension_tolerance = 0.01  # TODO: this should be proportional to the unit used
-        self.main_beam.add_blank_extension(start_main + extension_tolerance, end_main + extension_tolerance, self.key)
-        self.cross_beam.add_blank_extension(
+        self.beam_a.add_blank_extension(start_main + extension_tolerance, end_main + extension_tolerance, self.key)
+        self.beam_b.add_blank_extension(
             start_cross + extension_tolerance, end_cross + extension_tolerance, self.key
         )
 
-        self.main_beam.add_features(MillVolume(negative_brep_main_beam))
-        self.cross_beam.add_features(MillVolume(negative_brep_cross_beam))
+        self.beam_a.add_features(MillVolume(negative_brep_beam_a))
+        self.beam_b.add_features(MillVolume(negative_brep_beam_b))
 
         f_cross = CutFeature(cross_cutting_frame)
-        self.cross_beam.add_features(f_cross)
+        self.beam_b.add_features(f_cross)
         self.features.append(f_cross)
 
         trim_frame = Frame(main_cutting_frame.point, main_cutting_frame.xaxis, -main_cutting_frame.yaxis)
         f_main = CutFeature(trim_frame)
-        self.main_beam.add_features(f_main)
+        self.beam_a.add_features(f_main)
         self.features.append(f_main)
