90efe89e1554e0c3333fc2e6ab844d47c4c47f2a
Chen Kasirer <ckasirer@ethz.ch>
2024-03-20 22:25:05 +0100

    import assembly component

--- src/compas_timber/ghpython/components/CT_Import/code.py
+++ src/compas_timber/ghpython/components/CT_Import/code.py
@@ -2,15 +2,25 @@ from ghpythonlib.componentbase import executingcomponent as component
 from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
 
 from compas.data import json_load
+from compas.scene import Scene
+from compas_timber.consumers import BrepGeometryConsumer
 
+from scriptcontext import sticky
 
-class ExportTimberAssembly(component):
+class ImportTimberAssembly(component):
 
     def RunScript(self, filepath, import_):
-
         if not filepath:
             self.AddRuntimeMessage(Warning, "Input parameter Filepath failed to collect data")
-        if not import_:
-            return
 
-        return json_load(filepath)
+        KEY = "assembly"
+        if import_:
+            sticky[KEY] = json_load(filepath)
+        scene = Scene()
+        assembly = sticky.get(KEY, None)
+        if assembly:
+            consumer = BrepGeometryConsumer(assembly)
+            for result in consumer.result:
+                scene.add(result.geometry)
+
+        return assembly, scene.draw()
