bfedb35ed8c2aca206ae9b541f123a69b5363b96
obucklin <obucklin@gmail.com>
2024-03-27 19:03:55 +0100

    ciao for now

--- src/compas_timber/connections/new_lap_joint.py
+++ src/compas_timber/connections/new_lap_joint.py
@@ -1,4 +1,4 @@
-from ast import main
+
 from compas.geometry import Frame
 from compas.geometry import Line
 from compas.geometry import Plane
@@ -153,165 +153,45 @@ class NewLapJoint(Joint):
         return cfr
 
     def generate_intersect_geometry(self):
-        print("get_intersect_points")
+        self.main_intersecting_geometry = self.intersecting_geometry(self.main_beam, self.cross_beam)
+        self.cross_intersecting_geometry = self.intersecting_geometry(self.cross_beam, self.main_beam)
+
+
+    @ classmethod
+    def intersecting_geometry(self, face_beam, edge_beam):
+        intersecting_geometry = {
+            "beam": face_beam,
+            "faces": [],
+            "face_points": {},
+            "edges": [],
+            }
+        face_indices = []
+        edge_indices = []
         points = []
-        self.main_intersecting_geometry = {
-            "beam": self.main_beam ,
-            "faces": [],
-            "face_points": {},
-            "edges": [],
-        }
-        self.cross_intersecting_geometry = {
-            "beam": self.cross_beam,
-            "faces": [],
-            "face_points": {},
-            "edges": [],
-            }
-        face_indices = []
-        edge_indices = []
-        for i, face in enumerate(self.main_beam.faces[0:4]):
+        for i, face in enumerate(face_beam.faces[0:4]):
             face_pts = []
-            for j, edge in enumerate(self.cross_beam.long_edges):
+            for j, edge in enumerate(edge_beam.long_edges):
                 point = intersection_line_plane(edge, Plane.from_frame(face))
                 if point:
-
                     pt_xy = Point(*point)
                     pt_xy.transform(Transformation.from_frame_to_frame(face, Frame.worldXY()))
                     if i % 2 == 0:
-                        if (pt_xy.x > 0) and (pt_xy.x < self.main_beam.blank_length)  and (pt_xy.y> 0) and (pt_xy.y < self.main_beam.width):
+                        if (pt_xy.x > 0) and (pt_xy.x < face_beam.blank_length)  and (pt_xy.y > 0) and (pt_xy.y < face_beam.width):
                             points.append(Point(*point))
                             face_indices.append(i)
                             edge_indices.append(j)
                             face_pts.append(point)
                     else:
-                        if (pt_xy.x > 0) and (pt_xy.x < self.main_beam.blank_length)  and (pt_xy.y> 0) and (pt_xy.y < self.main_beam.height):
+                        if (pt_xy.x > 0) and (pt_xy.x < face_beam.blank_length) and (pt_xy.y > 0) and (pt_xy.y < face_beam.height):
                             points.append(Point(*point))
                             face_indices.append(i)
                             edge_indices.append(j)
                             face_pts.append(point)
             if face_pts:
-                self.cross_intersecting_geometry["face_points"][i] = face_pts
-        self.main_intersecting_geometry["faces"] = set([self.main_beam.faces[i] for i in face_indices])
-        self.cross_intersecting_geometry["edges"] = set([self.cross_beam.long_edges[i] for i in edge_indices])
+                intersecting_geometry["face_points"][i] = face_pts
+        return intersecting_geometry
 
 
-        for i, face in enumerate(self.cross_beam.faces[0:4]):
-            for j, edge in enumerate(self.main_beam.long_edges):
-                point = intersection_line_plane(edge, Plane.from_frame(face))
-                if point:
-                    pt_xy = Point(*point)
-                    pt_xy.transform(Transformation.from_frame_to_frame(face, Frame.worldXY()))
-                    if i%2 == 0:
-                        if pt_xy.x > 0 and pt_xy.x < self.cross_beam.blank_length  and pt_xy.y> 0 and pt_xy.y < self.cross_beam.width:
-                            points.append(Point(*point))
-                            face_indices.append(i)
-                            edge_indices.append(j)
-                            self.main_intersecting_geometry["edge_points"].append(point)
-                    else:
-                        if pt_xy.x > 0 and pt_xy.x < self.cross_beam.blank_length  and pt_xy.y> 0 and pt_xy.y < self.cross_beam.height:
-                            points.append(Point(*point))
-                            face_indices.append(i)
-                            edge_indices.append(j)
-                            self.main_intersecting_geometry["edge_points"].append(point)
-        self.cross_intersecting_geometry["faces"] = set([self.cross_beam.faces[i] for i in face_indices])
-        self.main_intersecting_geometry["edges"] = set([self.main_beam.long_edges[i] for i in edge_indices])
-        return points
-
-
-    def intersecting_geometry
-        self.cross_intersecting_geometry = {
-            "beam": self.cross_beam,
-            "faces": [],
-            "face_points": {},
-            "edges": [],
-            }
-        face_indices = []
-        edge_indices = []
-        for i, face in enumerate(self.main_beam.faces[0:4]):
-            face_pts = []
-            for j, edge in enumerate(self.cross_beam.long_edges):
-                point = intersection_line_plane(edge, Plane.from_frame(face))
-                if point:
-
-                    pt_xy = Point(*point)
-                    pt_xy.transform(Transformation.from_frame_to_frame(face, Frame.worldXY()))
-                    if i % 2 == 0:
-                        if (pt_xy.x > 0) and (pt_xy.x < self.main_beam.blank_length)  and (pt_xy.y> 0) and (pt_xy.y < self.main_beam.width):
-                            points.append(Point(*point))
-                            face_indices.append(i)
-                            edge_indices.append(j)
-                            face_pts.append(point)
-                    else:
-                        if (pt_xy.x > 0) and (pt_xy.x < self.main_beam.blank_length)  and (pt_xy.y> 0) and (pt_xy.y < self.main_beam.height):
-                            points.append(Point(*point))
-                            face_indices.append(i)
-                            edge_indices.append(j)
-                            face_pts.append(point)
-            if face_pts:
-                self.cross_intersecting_geometry["face_points"][i] = face_pts
-
-
-
-
-    # def parse_intersections(self):
-    #     self.main_vol = None
-    #     self.cross_vol = None
-    #     if len(self.intersecting_geometry["edges"]) == 1:
-    #         if len(self.cross_intersecting_geometry["edges"]) == 1: # diagonal-diagonal  crazy
-    #             print("diagonal-diagonal")
-
-
-
-    #             self.generate_diagonal_diagonal_vols()
-
-    #             return
-    #         elif len(self.intersecting_geometry["edges"]) == 2: # flat-diagonal
-    #             self.generate_flat_diagonal_vols()
-
-    #         elif len(self.intersecting_geometry["edges"]) > 2: # diagonal-embedded
-    #             print("diagonal-embedded")
-    #             return
-
-    #     elif len(self.intersecting_geometry["edges"]) == 2:
-    #         if len(self.intersecting_geometry["edges"]) == 1: # flat-diagonal
-    #             self.generate_flat_diagonal_vols()
-
-    #         elif len(self.intersecting_geometry["edges"]) == 2: # flat-flat
-    #             self.generate_flat_flat_vols()
-
-    #         elif len(self.intersecting_geometry["edges"]) > 2: # flat-embedded
-    #             self.generate_embedded_vols()
-    #             return
-
-    #     elif len(self.intersecting_geometry["edges"]) == 3:
-    #         if len(self.intersecting_geometry["edges"]) == 1: # embedded-diagonal
-    #             print("embedded-diagonal")
-    #             return
-    #         elif len(self.intersecting_geometry["edges"]) == 2: # embedded-flat
-    #             self.generate_embedded_vols()
-    #             return
-    #         elif len(self.intersecting_geometry["edges"]) > 2: # embedded-embedded
-    #             print("embedded-embedded")
-    #             return
-
-    #     elif len(self.intersecting_geometry["edges"]) == 4:
-    #         if len(self.intersecting_geometry["faces"]) > 1: # embedded-embedded
-    #             print("cfi len = ", len(self.intersecting_geometry["faces"]))
-    #             if abs(self.intersecting_geometry["faces"][0] - self.intersecting_geometry["faces"][-1]) == 2 or len(self.intersecting_geometry["faces"]) == 3:
-    #                 print("embedded-flat")
-    #                 self.generate_through_vols()
-    #             else:
-    #                 print("THIS embedded-diagonal")
-    #                 return
-    #         if len(self.intersecting_geometry["edges"]) == 1: # embedded-diagonal
-    #             print("embedded-diagonal")
-    #             return
-    #         elif len(self.intersecting_geometry["edges"]) == 2: # embedded-flat
-    #             self.generate_embedded_vols()
-    #             return
-    #         elif len(self.intersecting_geometry["edges"]) > 2: # embedded-embedded
-    #             print("embedded-embedded")
-    #             return
 
     def get_diagonal_plane(self):
         pass
@@ -356,48 +236,8 @@ class NewLapJoint(Joint):
             pass
 
 
-    def get_diagonal_diagonal_plane(self):
-        print("DID diagonal-diagonal")
-        pts = intersection_line_line(self.main_beam.long_edges[self.intersecting_geometry["edges"][0]], self.cross_beam.long_edges[self.intersecting_geometry["edges"][0]])
-        pt = Point(*pts[0])* self.cut_plane_bias + Point(*pts[1]) * (1-self.cut_plane_bias)
-        return Plane(pt, Vector.from_start_end(pts[1], pts[0]))
 
 
-
-    def get_flat_diagonal_plane(self):
-        if len(self.intersecting_geometry["edge_points"]) == 4:
-            pts = self.intersecting_geometry["edge_points"][0:3]
-            pt = self.cross_beam.long_edges[self.intersecting_geometry["edges"][0]].start
-            flip = False
-        else:
-            pts = self.intersecting_geometry["edge_points"][0:3]
-            pt = self.main_beam.long_edges[self.intersecting_geometry["edges"][0]].start
-            flip = True
-        vector = cross_vectors(self.cross_beam.centerline.vector, self.main_beam.centerline.vector)
-        diagonal_plane = Plane(pt, vector)
-        pts_projected = []
-        bias = self.cut_plane_bias if flip else 1 - self.cut_plane_bias
-        for point in pts[0:3]:
-            pt_proj = closest_point_on_plane(point, diagonal_plane)
-            pts_projected.append(Point(*pt_proj) * self.cut_plane_bias + Point(*point) * (1-self.cut_plane_bias))
-        print(pts_projected)
-        self.cut_plane = Plane.from_three_points(*[Point(*cut_point) for cut_point in pts_projected])
-        self.generate_sub_vols_from_cut_plane()
-
-
-
-    def generate_flat_flat_vols(self):
-        main_pts = self.intersecting_geometry["edge_points"][0:3]
-        cross_pts = self.intersecting_geometry["edge_points"][0:3]
-        cross_plane = Plane.from_three_points(*[Point(*cut_point) for cut_point in cross_pts])
-
-        cut_pts = []
-        for point in main_pts[0:3]:
-            pt_proj = closest_point_on_plane(point, cross_plane)
-            cut_pts.append(Point(*pt_proj) * self.cut_plane_bias + Point(*point) * (1-self.cut_plane_bias))
-        print(cut_pts)
-        self.cut_plane = Plane.from_three_points(*[Point(*cut_point) for cut_point in cut_pts])
-        self.generate_sub_vols_from_cut_plane()
 
 
     @property
@@ -420,57 +260,23 @@ class NewLapJoint(Joint):
 
 
 
-    def generate_vols(self):
+    def beam_bool_intersect(self, beam_a, beam_b):
+
+        intersecting_geometry_a = self.intersecting_geometry(self.main_beam, self.cross_beam)
+        intersecting_geometry_b = self.intersecting_geometry(self.cross_beam, self.main_beam)
 
 
 
 
 
 
-    def generate_standard_vols(self):
-        print("embedded")
-        beam_a, beam_b = self.beams
-        assert beam_a and beam_b
 
-        # Get Cut Plane
-        plane_cut_vector = beam_a.centerline.vector.cross(beam_b.centerline.vector)
-
-        if self.flip_lap_side:
-            plane_cut_vector = -plane_cut_vector
-
-        # Get Beam Faces (Planes) in right order
-        planes_main = self._sort_beam_planes(beam_a, plane_cut_vector)
-        plane_a0, plane_a1, plane_a2, plane_a3 = planes_main
-
-        planes_cross = self._sort_beam_planes(beam_b, -plane_cut_vector)
-        plane_b0, plane_b1, plane_b2, plane_b3 = planes_cross
-
-        # Lines as Frame Intersections
-        lines = []
-
-        pt_a = intersection_plane_plane_plane(plane_a1, plane_b1, plane_a0)
-        pt_b = intersection_plane_plane_plane(plane_a1, plane_b1, plane_b0)
-        lines.append(Line(pt_a, pt_b))
-
-        pt_a = intersection_plane_plane_plane(plane_a1, plane_b2, plane_a0)
-        pt_b = intersection_plane_plane_plane(plane_a1, plane_b2, plane_b0)
-        lines.append(Line(pt_a, pt_b))
-
-        pt_a = intersection_plane_plane_plane(plane_a2, plane_b2, plane_a0)
-        pt_b = intersection_plane_plane_plane(plane_a2, plane_b2, plane_b0)
-        lines.append(Line(pt_a, pt_b))
-
-        pt_a = intersection_plane_plane_plane(plane_a2, plane_b1, plane_a0)
-        pt_b = intersection_plane_plane_plane(plane_a2, plane_b1, plane_b0)
-        lines.append(Line(pt_a, pt_b))
-
-        # Create Polyhedrons
-        self.cross_vol =  self._create_polyhedron(plane_a0, lines, self.cut_plane_bias)
-        self.main_vol = self._create_polyhedron(plane_b0, lines, self.cut_plane_bias)
 
     def add_features(self):
         self.generate_intersect_geometry()
-        self.parse_intersections()
+
+
+
 
         if self.main_vol:
             if isinstance(self.main_vol, Brep):
