8581c82e3df7c1dda6a2beeefab99cf8a5483540
Chen Kasirer <ckasirer@ethz.ch>
2024-06-11 17:15:56 +0200

    assembly -> model in solver pluggins

--- src/compas_timber/solvers/__init__.py
+++ src/compas_timber/solvers/__init__.py
@@ -25,50 +25,50 @@ from compas.plugins import pluggable
 
 
 @pluggable(category="solvers")
-def next_removable_part(assembly, removed_part_ids, part_ids_to_remove, number_of_parts_to_remove, **kwargs):
-    """Returns the next part to remove from the assembly.
+def next_removable_part(model, removed_part_ids, part_ids_to_remove, number_of_parts_to_remove, **kwargs):
+    """Returns the next part to remove from the model.
 
-    One or more parts of the assembly are choosen to be removed next based on the particular solver implementation.
+    One or more parts of the model are choosen to be removed next based on the particular solver implementation.
 
     Parameters
     ----------
-    assembly : `compas.datastructures.Assembly`
-        The assembly to remove parts from.
+    model : `compas_model.model.Model`
+        The model to remove parts from.
     removed_part_ids : list
         The list of part ids that have already been removed.
     part_ids_to_remove : list
         The list of part ids that are to be removed.
     number_of_parts_to_remove : int
-        The number of parts to remove from the assembly.
+        The number of parts to remove from the model.
 
     Returns
     -------
     :class:`compas_monosashi.sequencer.Step`
         A step which contains one or more Instructions which specify
-        the parts to remove from the assembly and the corresponding removal direction.
+        the parts to remove from the model and the corresponding removal direction.
 
     """
     raise PluginNotInstalledError
 
 
 @pluggable(category="solvers")
-def create_dissassembly_sequence(assembly, **kwargs):
-    """Returns a sequence of steps to disassemble the assembly.
+def create_dissassembly_sequence(model, **kwargs):
+    """Returns a sequence of steps to disassemble the model.
 
     This function's implementation decides on the strategy used to generate the collection of
-    steps to disassemble the assembly and determines their order.
+    steps to disassemble the model and determines their order.
 
-    The order of steps can be revered in order to generate a sequence of steps to assemble the assembly.
+    The order of steps can be revered in order to generate a sequence of steps to assemble the model.
 
     Parameters
     ----------
-    assembly : `compas.datastructures.Assembly`
-        The assembly to disassemble.
+    model : `compas_model.model.Model`
+        The model to disassemble.
 
     Returns
     -------
     :class:`compas_monosashi.sequencer.BuildingPlan`
-        A sequence of steps to disassemble the assembly.
+        A sequence of steps to disassemble the model.
 
     See Also
     --------
