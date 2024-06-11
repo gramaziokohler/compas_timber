d7d001725403e80cdf0999b071debbac0925b353
Chen Kasirer <ckasirer@ethz.ch>
2024-06-11 16:18:39 +0200

    assembly -> model

--- tests/compas_timber/test_sequencer.py
+++ tests/compas_timber/test_sequencer.py
@@ -9,44 +9,44 @@ from compas_timber.planning import SimpleSequenceGenerator
 
 
 @pytest.fixture
-def mock_assembly():
-    assembly = TimberModel()
+def mock_model():
+    model = TimberModel()
     b1 = Beam(Frame.worldXY(), length=2.0, width=0.1, height=0.1)
     b2 = Beam(Frame.worldXY(), length=2.0, width=0.1, height=0.1)
     b3 = Beam(Frame.worldXY(), length=2.0, width=0.1, height=0.1)
     b4 = Beam(Frame.worldXY(), length=2.0, width=0.1, height=0.1)
     b5 = Beam(Frame.worldXY(), length=2.0, width=0.1, height=0.1)
     for b in [b1, b2, b3, b4, b5]:
-        assembly.add_beam(b)
-    return assembly
+        model.add_beam(b)
+    return model
 
 
-def test_simple_sequence_generator(mock_assembly):
-    generator = SimpleSequenceGenerator(mock_assembly)
+def test_simple_sequence_generator(mock_model):
+    generator = SimpleSequenceGenerator(mock_model)
     plan = generator.result
 
-    assert len(plan) == len(mock_assembly.beams)
-    for step, beam in zip(plan, mock_assembly.beams):
+    assert len(plan) == len(mock_model.beams)
+    for step, beam in zip(plan, mock_model.beams):
         assert str(beam.guid) == step.element_ids[0]
 
 
-def test_simple_sequence_generator_get_beam(mock_assembly):
-    generator = SimpleSequenceGenerator(mock_assembly)
+def test_simple_sequence_generator_get_beam(mock_model):
+    generator = SimpleSequenceGenerator(mock_model)
     plan = generator.result
 
-    assert len(plan) == len(mock_assembly.beams)
-    for step, beam in zip(plan, mock_assembly.beams):
+    assert len(plan) == len(mock_model.beams)
+    for step, beam in zip(plan, mock_model.beams):
         beam_guid = step.element_ids[0]
-        assert beam is mock_assembly.beam_by_guid(beam_guid)
+        assert beam is mock_model.beam_by_guid(beam_guid)
 
 
-def test_serialize_plan(mock_assembly):
-    generator = SimpleSequenceGenerator(mock_assembly)
+def test_serialize_plan(mock_model):
+    generator = SimpleSequenceGenerator(mock_model)
     plan = generator.result
 
     plan = json_loads(json_dumps(plan))
-    assembly = json_loads(json_dumps(mock_assembly))
+    model = json_loads(json_dumps(mock_model))
 
-    assert len(plan) == len(assembly.beams)
-    for step, beam in zip(plan, assembly.beams):
+    assert len(plan) == len(model.beams)
+    for step, beam in zip(plan, model.beams):
         assert str(beam.guid) == step.element_ids[0]
