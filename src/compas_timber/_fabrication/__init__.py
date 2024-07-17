d01155c02b997a45ded1dd56a3474a0cde42f15d
Chen Kasirer <ckasirer@ethz.ch>
2024-07-17 15:59:22 +0200

    added orientation type to 2nd level import

--- src/compas_timber/_fabrication/__init__.py
+++ src/compas_timber/_fabrication/__init__.py
@@ -1,7 +1,8 @@
 # This module is a tepmorary solution to the problem of circular imports in the compas_timber package.
 # It will be reoved or merged with the `fabrication` module once the migration to the new feature system is complete.
 from .btlx_process import BTLxProcess
+from .btlx_process import OrientationType
 from .jack_cut import JackRafterCut
 
 
-__all__ = ["JackRafterCut", "BTLxProcess"]
+__all__ = ["JackRafterCut", "BTLxProcess", "OrientationType"]
