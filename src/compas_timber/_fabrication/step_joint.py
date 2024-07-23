a686051c32994efbc4e0b03b3eedb6a216fded17
papachap <ppapachar@gmail.com>
2024-07-23 17:27:00 +0200

    initialization of step_joint_notch and method from surface and beam

--- src/compas_timber/_fabrication/step_joint.py
+++ src/compas_timber/_fabrication/step_joint.py
@@ -6,10 +6,13 @@ from compas.geometry import Line
 from compas.geometry import Plane
 from compas.geometry import Rotation
 from compas.geometry import Vector
+from compas.geometry import Surface
+from compas.geometry import PlanarSurface
 from compas.geometry import angle_vectors_signed
 from compas.geometry import distance_point_point
 from compas.geometry import intersection_line_plane
 from compas.geometry import is_point_behind_plane
+
 
 from compas_timber.elements import FeatureApplicationError
 
@@ -17,8 +20,8 @@ from .btlx_process import BTLxProcess
 from .btlx_process import OrientationType
 
 
-class StepJoint(BTLxProcess):
-    """Represents a Step Joint feature to be made on a beam.
+class StepJointNotch(BTLxProcess):
+    """Represents a Step Joint Notch feature to be made on a beam.
 
     Parameters
     ----------
@@ -26,58 +29,78 @@ class StepJoint(BTLxProcess):
         The orientation of the cut. Must be either OrientationType.START or OrientationType.END.
     start_x : float
         The start x-coordinate of the cut in parametric space of the reference side. -100000.0 < start_x < 100000.0.
+    start_y : float
+        The start y-coordinate of the notch in parametric space of the reference side. -50000.0 < start_y < 50000.0.
     strut_inclination : float
         The inclination of the strut. 0.1 < strut_inclination < 179.9.
+    notch_limited : bool
+        Whether the notch is limited. If True, the notch is limited by the start_y and notch_width values.
+    notch_width : float
+        The width of the notch. notch_width < 50000.0.
     step_depth : float
-        The depth of the step. 0.0 < step_depth < 50000.0.
+        The depth of the step. step_depth < 50000.0.
     heel_depth : float
-        The depth of the heel. 0.0 < heel_depth < 50000.0.
+        The depth of the heel. heel_depth < 50000.0.
+    strut_height : float
+        The height of the strut. It is the cross beam's height. strut_height < 50000.0.
     step_shape : str
         The shape of the step. Must be either 'double', 'step', 'heel', or 'taperedheel'.
-    tenon : str
-        The presence of a tenon. Must be either 'no' or 'yes'.
-    tenon_width : float
-        The width of the tenon. 0.0 < tenon_width < 1000.0.
-    tenon_height : float
-        The height of the tenon. 0.0 < tenon_height < 1000.0.
+    mortise : str
+        The presence of a mortise. Must be either 'no' or 'yes'.
+    mortise_width : float
+        The width of the mortise. mortise_width < 1000.0.
+    mortise_height : float
+        The height of the mortise. mortise_height < 1000.0.
 
     """
 
     @property
     def __data__(self):
-        data = super(StepJoint, self).__data__
+        data = super(StepJointNotch, self).__data__
         data["orientation"] = self.orientation
         data["start_x"] = self.start_x
+        data["start_y"] = self.start_y
         data["strut_inclination"] = self.strut_inclination
+        data["notch_limited"] = self.notch_limited
+        data["notch_width"] = self.notch_width
         data["step_depth"] = self.step_depth
         data["heel_depth"] = self.heel_depth
+        data["strut_height"] = self.strut_height
         data["step_shape"] = self.step_shape
-        data["tenon"] = self.tenon
-        data["tenon_width"] = self.tenon_width
-        data["tenon_height"] = self.tenon_height
+        data["mortise"] = self.mortise
+        data["mortise_width"] = self.mortise_width
+        data["mortise_height"] = self.mortise_height
         return data
 
-    def __init__(self, orientation, start_x=0.0, strut_inclination=90.0, step_depth=20.0, heel_depth=20.0, step_shape="double", tenon="no", tenon_width=40.0, tenon_height=40.0, **kwargs):
-        super(StepJoint, self).__init__(**kwargs)
+    def __init__(self, orientation, start_x=0.0, start_y=0.0, strut_inclination=90.0, notch_limited="no", notch_width=20.0, step_depth=20.0, heel_depth=20.0, strut_height=20.0, step_shape="double", mortise="no", mortise_width=40.0, mortise_height=40.0, **kwargs):
+        super(StepJointNotch, self).__init__(**kwargs)
         self._orientation = None
         self._start_x = None
+        self._start_y = None
         self._strut_inclination = None
+        self._notch_limited = None
+        self._notch_width = None
         self._step_depth = None
         self._heel_depth = None
+        self._strut_height = None
         self._step_shape = None
-        self._tenon = None
-        self._tenon_width = None
-        self._tenon_height = None
+        self._mortise = None
+        self._mortise_width = None
+        self._mortise_height = None
 
         self.orientation = orientation
         self.start_x = start_x
+        self.start_y = start_y
         self.strut_inclination = strut_inclination
+        self.notch_limited = notch_limited
+        self.notch_width = notch_width
         self.step_depth = step_depth
         self.heel_depth = heel_depth
+        self.strut_height = strut_height
         self.step_shape = step_shape
-        self.tenon = tenon
-        self.tenon_width = tenon_width
-        self.tenon_height = tenon_height
+        self.mortise = mortise
+        self.mortise_width = mortise_width
+        self.mortise_height = mortise_height
 
     ########################################################################
     # Properties
@@ -104,6 +127,16 @@ class StepJoint(BTLxProcess):
         self._start_x = start_x
 
     @property
+    def start_y(self):
+        return self._start_y
+
+    @start_y.setter
+    def start_y(self, start_y):
+        if start_y > 50000.0 or start_y < -50000.0:
+            raise ValueError("StartY must be between -50000.0 and 50000.")
+        self._start_y = start_y
+
+    @property
     def strut_inclination(self):
         return self._strut_inclination
 
@@ -112,6 +145,26 @@ class StepJoint(BTLxProcess):
         if strut_inclination < 0.1 or strut_inclination > 179.9:
             raise ValueError("StrutInclination must be between 0.1 and 179.9.")
         self._strut_inclination = strut_inclination
+
+    @property
+    def notch_limited(self):
+        return self._notch_limited
+
+    @notch_limited.setter
+    def notch_limited(self, notch_limited):
+        if notch_limited not in ["no", "yes"]:
+            raise ValueError("NotchLimited must be either 'no' or 'yes'.")
+        self._notch_limited = notch_limited
+
+    @property
+    def notch_width(self):
+        return self._notch_width
+
+    @notch_width.setter
+    def notch_width(self, notch_width):
+        if notch_width > 50000.0:
+            raise ValueError("NotchWidth must be less than 50000.0.")
+        self._notch_width = notch_width
 
     @property
     def step_depth(self):
@@ -134,57 +187,67 @@ class StepJoint(BTLxProcess):
         self._heel_depth = heel_depth
 
     @property
+    def strut_height(self):
+        return self._strut_height
+
+    @strut_height.setter
+    def strut_height(self, strut_height):
+        if strut_height > 50000.0:
+            raise ValueError("StrutHeight must be less than 50000.0.")
+        self._strut_height = strut_height
+
+    @property
     def step_shape(self):
         return self._step_shape
 
-    @step_shape.setter
+    @step_shape.setter #TODO: should this be defined automatically depending on the other parameters? (ie. if heel_depth > 0 and step_depth > 0, then step_shape = "double")
     def step_shape(self, step_shape):
         if step_shape not in ["double", "step", "heel", "taperedheel"]:
             raise ValueError("StepShape must be either 'double', 'step', 'heel', or 'taperedheel'.")
         self._step_shape = step_shape
 
     @property
-    def tenon(self):
-        return self._tenon
+    def mortise(self):
+        return self._mortise
 
-    @tenon.setter
-    def tenon(self, tenon):
-        if tenon not in ["no", "yes"]:
-            raise ValueError("Tenon must be either 'no' or 'yes'.")
-        self._tenon = tenon
-
-    @property
-    def tenon_width(self):
-        return self._tenon_width
-
-    @tenon_width.setter
-    def tenon_width(self, tenon_width):
-        if tenon_width > 1000.0:
-            raise ValueError("TenonWidth must be less than 1000.0.")
-        self._tenon_width = tenon_width
+    @mortise.setter
+    def mortise(self, mortise):
+        if mortise not in ["no", "yes"]:
+            raise ValueError("Mortise must be either 'no' or 'yes'.")
+        self._mortise = mortise
 
     @property
-    def tenon_height(self):
-        return self._tenon_height
+    def mortise_width(self):
+        return self._mortise_width
 
-    @tenon_height.setter
-    def tenon_height(self, tenon_height):
-        if tenon_height > 1000.0:
-            raise ValueError("TenonHeight must be less than 1000.0.")
-        self._tenon_height = tenon_height
+    @mortise_width.setter
+    def mortise_width(self, mortise_width):
+        if mortise_width > 1000.0:
+            raise ValueError("MortiseWidth must be less than 1000.0.")
+        self._mortise_width = mortise_width
+
+    @property
+    def mortise_height(self):
+        return self._mortise_height
+
+    @mortise_height.setter
+    def mortise_height(self, mortise_height):
+        if mortise_height > 1000.0:
+            raise ValueError("MortiseHeight must be less than 1000.0.")
+        self._mortise_height = mortise_height
 
     ########################################################################
     # Alternative constructors
     ########################################################################
 
     @classmethod
-    def from_plane_and_beam(cls, plane, beam, ref_side_index=0):
-        """Create a JackRafterCut instance from a cutting plane and the beam it should cut.
+    def from_surface_and_beam(cls, surface, beam, notch_limited=False, step_depth=0.0, heel_depth=0.0, tapered_heel=False, mortise_height=0.0, ref_side_index=0):
+        """Create a StepJointNotch instance from a cutting surface and the beam it should cut.
 
         Parameters
         ----------
-        plane : :class:`~compas.geometry.Plane` or :class:`~compas.geometry.Frame`
-            The cutting plane.
+        surface : :class:`~compas.geometry.PlanarSurface` or :class:`~compas.geometry.Surface`
+            The cutting surface.
         beam : :class:`~compas_timber.elements.Beam`
             The beam that is cut by this instance.
         ref_side_index : int, optional
@@ -192,26 +255,48 @@ class StepJoint(BTLxProcess):
 
         Returns
         -------
-        :class:`~compas_timber.fabrication.JackRafterCut`
+        :class:`~compas_timber.fabrication.StepJointNotch`
 
         """
-        # type: (Plane | Frame, Beam, int) -> JackRafterCut
-        if isinstance(plane, Frame):
-            plane = Plane.from_frame(plane)
-        start_y = 0.0
-        start_depth = 0.0
+        # type: (PlanarSurface | Surface, Beam, bool, float, float, bool, float, int) -> StepJointNotch
+
+        # define ref_side & ref_edge
         ref_side = beam.ref_sides[ref_side_index]  # TODO: is this arbitrary?
         ref_edge = Line.from_point_and_vector(ref_side.point, ref_side.xaxis)
-        orientation = cls._calculate_orientation(ref_side, plane)
+        plane = Plane.from_frame(surface.frame_at())
 
+        # calculate orientation
+        orientation = cls._calculate_orientation(ref_side, plane)
+        # calculate start_x
         point_start_x = intersection_line_plane(ref_edge, plane)
         if point_start_x is None:
             raise ValueError("Plane does not intersect with beam.")
-
         start_x = distance_point_point(ref_edge.point, point_start_x)
-        angle = cls._calculate_angle(ref_side, plane, orientation)
-        inclination = cls._calculate_inclination(ref_side, plane, orientation)
-        return cls(orientation, start_x, start_y, start_depth, angle, inclination, ref_side_index=ref_side_index)
+        # calculate start_y
+        point_start_y = ref_side.intersections_with_surface(surface)[0]
+        if point_start_y is None:
+            raise ValueError("Surface does not intersect with beam.")
+        start_y = distance_point_point(point_start_x, point_start_y)
+        # calculate strut_inclination
+        strut_inclination = cls._calculate_strut_inclination(ref_side, plane, orientation)
+        # calculate notch_width
+        if notch_limited == True:
+            notch_width = surface.ysize
+            notch_limited = "yes"
+        else:
+            notch_width = beam.width
+            notch_limited = "no"
+        # restrain step_depth & heel_depth #TODO: should those be defined automatically based on the angle?
+        step_depth = beam.height/2 if step_depth > beam.height/2 else step_depth # TODO: should it be constrained?
+        heel_depth = beam.height/2 if heel_depth > beam.height/2 else heel_depth # TODO: should it be constrained?
+        # define step_shape
+        step_shape = cls._define_step_shape(step_depth, heel_depth, tapered_heel)
+        # define mortise
+        mortise, mortise_width, mortise_height = cls._calculate_mortise(beam, mortise_height)
+        # define strut_height
+        strut_height = surface.ysize #TODO: Wrong! should have been defined by the main beam height instead
+
+        return cls(orientation, start_x, start_y, strut_inclination, notch_limited, notch_width, step_depth, heel_depth, step_shape, mortise, mortise_width, mortise_width, strut_height)
 
     @staticmethod
     def _calculate_orientation(ref_side, cutting_plane):
@@ -223,24 +308,43 @@ class StepJoint(BTLxProcess):
             return OrientationType.START
 
     @staticmethod
-    def _calculate_angle(ref_side, plane, orientation):
+    def _calculate_strut_inclination(ref_side, plane, orientation):
         # vector rotation direction of the plane's normal in the vertical direction
-        angle_vector = Vector.cross(ref_side.zaxis, plane.normal)
-        angle = angle_vectors_signed(ref_side.xaxis, angle_vector, ref_side.zaxis, deg=True)
+        strut_inclination_vector = Vector.cross(ref_side.zaxis, plane.normal)
+        strut_inclination = angle_vectors_signed(ref_side.zaxis, plane.normal, strut_inclination_vector, deg=True)
         if orientation == OrientationType.START:
-            return 180 - abs(angle)  # get the other side of the angle
+            return 180 - abs(strut_inclination)  # get the other side of the angle
         else:
-            return abs(angle)
+            return abs(strut_inclination)
 
     @staticmethod
-    def _calculate_inclination(ref_side, plane, orientation):
-        # vector rotation direction of the plane's normal in the horizontal direction
-        inclination_vector = Vector.cross(ref_side.yaxis, plane.normal)
-        inclination = angle_vectors_signed(ref_side.xaxis, inclination_vector, ref_side.yaxis, deg=True)
-        if orientation == OrientationType.START:
-            return 180 - abs(inclination)  # get the other side of the angle
+    def _define_step_shape(step_depth, heel_depth, tapered_heel):
+        # step_shape based on step_depth and heel_depth variables
+        if step_depth > 0.0 and heel_depth == 0.0:
+            step_shape = "step"
+        elif step_depth == 0.0 and heel_depth > 0.0:
+            if tapered_heel:
+                step_shape = "heel_tapered"
+            else:
+                step_shape = "heel"
+        elif step_depth > 0.0 and heel_depth > 0.0:
+            step_shape = "double"
         else:
-            return abs(inclination)
+            raise ValueError("at least one of step_depth or heel_depth must be greater than 0.0.")
+        return step_shape
+
+    @staticmethod
+    def _calculate_mortise(beam, mortise_height):
+        # mortise based on mortise_height variable and beam dimensions
+        if mortise_height > 0.0:
+            mortise = "yes"
+            mortise_width = beam.width/4 #TODO: should this be predefined? Also 1/3 or 1/4?
+            mortise_height = beam.height if mortise_height > beam.height else mortise_height
+        else:
+            mortise = "no"
+            mortise_width = 0.0
+        return mortise, mortise_width, mortise_height
+
 
     ########################################################################
     # Methods
