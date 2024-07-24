017cc922c36a5d5a7a820cb858721abe750726d4
papachap <ppapachar@gmail.com>
2024-07-24 14:22:51 +0200

    move add mortise method down

--- src/compas_timber/_fabrication/step_joint.py
+++ src/compas_timber/_fabrication/step_joint.py
@@ -271,7 +271,7 @@ class StepJointNotch(BTLxProcess):
         point_start_x = intersection_line_plane(ref_edge, plane)
         if point_start_x is None:
             raise ValueError("Plane does not intersect with beam.")
-        start_x = distance_point_point(ref_edge.point, point_start_x)
+        start_x = distance_point_point(ref_side.point, point_start_x)
         # calculate start_y
         point_start_y = ref_side.intersections_with_surface(surface)[0]
         if point_start_y is None:
@@ -295,22 +295,6 @@ class StepJointNotch(BTLxProcess):
         strut_height = surface.ysize #TODO: Wrong! should have been defined by the main beam height instead
 
         return cls(orientation, start_x, start_y, strut_inclination, notch_limited, notch_width, step_depth, heel_depth, step_shape, strut_height)
-
-    def add_mortise(self, mortise_width, mortise_height):
-        """Add a mortise to the existing StepJointNotch instance.
-
-        Parameters
-        ----------
-        mortise_width : float
-            The width of the mortise. mortise_width < 1000.0.
-        mortise_height : float
-            The height of the mortise. mortise_height < 1000.0.
-        """
-        self.mortise = "yes"
-        self.mortise_width = mortise_width
-        self.mortise_height = mortise_height
-        # self.mortise_width = beam.width / 4  # TODO: should this be related to a beam? 1/3 or 1/4 of beam.width?
-        # self.mortise_height = beam.height if mortise_height > beam.height else mortise_height #TODO: should this be constrained?
 
     @staticmethod
     def _calculate_orientation(ref_side, cutting_plane):
@@ -347,6 +331,30 @@ class StepJointNotch(BTLxProcess):
             raise ValueError("at least one of step_depth or heel_depth must be greater than 0.0.")
         return step_shape
 
+    @classmethod
+    def from_two_planes_and_beam(cls, plane_a, plane_b, beam, ref_side_index=0):
+        """Create a StepJointNotch instance from two planes and the beam it should cut.
+
+        Parameters
+        ----------
+        plane_a : :class:`~compas.geometry.Plane`
+            The first cutting plane.
+        plane_b : :class:`~compas.geometry.Plane`
+            The second cutting plane.
+        beam : :class:`~compas_timber.elements.Beam`
+            The beam that is cut by this instance.
+        ref_side_index : int, optional
+            The reference side index of the beam to be cut. Default is 0 (i.e. RS1).
+
+        Returns
+        -------
+        :class:`~compas_timber.fabrication.StepJointNotch`
+
+        """
+        # type: (Plane, Plane, Beam, int) -> StepJointNotch
+        # define ref_side & ref_edge
+        ref_side = beam.ref_sides[ref_side_index]
+
     ########################################################################
     # Methods
     ########################################################################
@@ -373,7 +381,7 @@ class StepJointNotch(BTLxProcess):
 
         """
         # type: (Brep, Beam) -> Brep
-        cutting_plane = self.plane_from_params_and_beam(beam)
+        cutting_planes = self.planes_from_params_and_beam(beam)
         try:
             return geometry.trimmed(cutting_plane)
         except BrepTrimmingError:
@@ -383,8 +391,24 @@ class StepJointNotch(BTLxProcess):
                 "The cutting plane does not intersect with beam geometry.",
             )
 
-    def plane_from_params_and_beam(self, beam):
-        """Calculates the cutting plane from the machining parameters in this instance and the given beam
+    def add_mortise(self, mortise_width, mortise_height):
+        """Add a mortise to the existing StepJointNotch instance.
+
+        Parameters
+        ----------
+        mortise_width : float
+            The width of the mortise. mortise_width < 1000.0.
+        mortise_height : float
+            The height of the mortise. mortise_height < 1000.0.
+        """
+        self.mortise = "yes"
+        self.mortise_width = mortise_width
+        self.mortise_height = mortise_height
+        # self.mortise_width = beam.width / 4  # TODO: should this be related to a beam? 1/3 or 1/4 of beam.width?
+        # self.mortise_height = beam.height if mortise_height > beam.height else mortise_height #TODO: should this be constrained?
+
+    def planes_from_params_and_beam(self, beam):
+        """Calculates the cutting planes from the machining parameters in this instance and the given beam
 
         Parameters
         ----------
@@ -394,27 +418,38 @@ class StepJointNotch(BTLxProcess):
         Returns
         -------
         :class:`compas.geometry.Plane`
-            The cutting plane.
+            The cutting planes.
 
         """
-        # type: (Beam) -> Plane
-        assert self.angle is not None
-        assert self.inclination is not None
+        # type: (Beam) -> Planes
+        assert self.strut_inclination is not None
+        assert self.step_shape is not None
+        assert self.strut_height is not None
 
-        # start with a plane aligned with the ref side but shifted to the start_x of the cut
-        ref_side = beam.side_as_surface(self.ref_side_index)
-        p_origin = ref_side.point_at(self.start_x, 0.0)
-        cutting_plane = Frame(p_origin, ref_side.frame.xaxis, ref_side.frame.yaxis)
+        # start with a plane aligned with the ref side but shifted to the start of the first cut
+        ref_side = beam.side_as_surface(self.ref_side_index) #TODO: missing a way of defining the ref_side_index for the class
 
-        # normal pointing towards xaxis so just need the delta
-        horizontal_angle = math.radians(self.angle - 90)
-        rot_a = Rotation.from_axis_and_angle(cutting_plane.zaxis, horizontal_angle, point=p_origin)
+        # orientation=start strut_inclination>90 #TODO: check if it applies to the other conditions
+        horizontal_angle = math.radians(self.strut_inclination - 90)
 
-        # normal pointing towards xaxis so just need the delta
-        vertical_angle = math.radians(self.inclination - 90)
-        rot_b = Rotation.from_axis_and_angle(cutting_plane.yaxis, vertical_angle, point=p_origin)
+        if self.step_type == "step":
+            #move the frames to the start and end of the notch to create the cuts
+            start_displacement = self.strut_height / math.sin(horizontal_angle)
+            p_origin = ref_side.point_at(self.start_x, self.start_y)
+            p_displaced = ref_side.point_at(self.start_x + start_displacement, self.start_y)
+            cutting_plane_a = Frame(p_displaced, ref_side.frame.xaxis, ref_side.frame.yaxis)
+            cutting_plane_b = Frame(p_origin, ref_side.frame.xaxis, ref_side.frame.yaxis)
 
-        cutting_plane.transform(rot_a * rot_b)
+            # rotate first cutting plane (small side of the step)
+            angle_a = math.tan(math.radians(self.strut_inclination/2))
+            rot_a = Rotation.from_axis_and_angle(-cutting_plane_a.yaxis, angle_a, point=p_displaced)
+            cutting_plane_a.transform(rot_a)
+
+            # rotate second cutting plane (large side of the step)
+            angle_b = math.atan(self.step_depth / (start_displacement - self.step_depth / math.tan(angle_a)))
+            rot_b = Rotation.from_axis_and_angle(cutting_plane_b.xaxis, angle_b, point=p_origin)
+            cutting_plane_b.transform(rot_b)
+
         # for simplicity, we always start with normal pointing towards xaxis.
         # if start is cut, we need to flip the normal
         if self.orientation == OrientationType.END:
