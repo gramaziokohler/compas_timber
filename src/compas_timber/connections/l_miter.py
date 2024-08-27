b8092e19876b3df7f2abf051ffe277019c19da15
Chen Kasirer <ckasirer@ethz.ch>
2024-08-27 18:46:56 +0200

    WIP: slot

--- src/compas_timber/connections/l_miter.py
+++ src/compas_timber/connections/l_miter.py
@@ -4,7 +4,9 @@ from compas.geometry import Point
 from compas.geometry import Vector
 from compas.geometry import cross_vectors
 
-from compas_timber._fabrication import JackRafterCut
+from compas_timber.elements import CutFeature
+
+# from compas_timber._fabrication import JackRafterCut
 from compas_timber.utils import intersection_line_line_3D
 
 from .joint import BeamJoinningError
@@ -123,8 +125,10 @@ class LMiterJoint(Joint):
 
         self.beam_a.add_blank_extension(start_a, end_a, self.guid)
         self.beam_b.add_blank_extension(start_b, end_b, self.guid)
-        cut1 = JackRafterCut.from_plane_and_beam(plane_a, self.beam_a)
-        cut2 = JackRafterCut.from_plane_and_beam(plane_b, self.beam_b)
+        # cut1 = JackRafterCut.from_plane_and_beam(plane_a, self.beam_a)
+        # cut2 = JackRafterCut.from_plane_and_beam(plane_b, self.beam_b)
+        cut1 = CutFeature(plane_a)
+        cut2 = CutFeature(plane_b)
         self.beam_a.add_features(cut1)
         self.beam_b.add_features(cut2)
         self.features = [cut1, cut2]
