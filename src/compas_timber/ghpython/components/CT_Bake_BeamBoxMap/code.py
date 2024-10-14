55be24e95ddca60377b3a19927a5f1f59ec04087
jonashaldemann <104436733+jonashaldemann@users.noreply.github.com>
2024-10-14 15:43:11 +0200

    handle list of models in GH Component

--- src/compas_timber/ghpython/components/CT_Bake_BeamBoxMap/code.py
+++ src/compas_timber/ghpython/components/CT_Bake_BeamBoxMap/code.py
@@ -7,15 +7,13 @@ import scriptcontext as sc
 import rhinoscriptsyntax as rs
 from compas_rhino.conversions import frame_to_rhino
 from ghpythonlib.componentbase import executingcomponent as component
-from Grasshopper.Kernel.GH_RuntimeMessageLevel import Error
-from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
+from Grasshopper.Kernel.GH_RuntimeMessageLevel import Error, Warning
 from Rhino import Render
-from Rhino.Geometry import Interval
-from Rhino.Geometry import Plane
+from Rhino.Geometry import Interval, Plane
 
 
 class BakeBeamBoxMap(component):
-    def RunScript(self, model, map_size, layer_name, clear_layer, bake):
+    def RunScript(self, model_list, map_size, layer_name, clear_layer_content, bake):
         if map_size and len(map_size) != 3:
             self.AddRuntimeMessage(
                 Error, "Input parameter MapSize requires exactly three float values (scale factors in x,y,z directions)"
@@ -25,12 +23,12 @@ class BakeBeamBoxMap(component):
         if map_size:
             dimx, dimy, dimz = map_size
         else:
-            # for the pine 251 material bitmap, rotated
+            # For the pine 251 material bitmap, rotated
             dimx = 0.2
             dimy = 0.2
             dimz = 1.0
 
-        if not model:
+        if not model_list:
             self.AddRuntimeMessage(Warning, "Input parameters Model failed to collect any Beam objects.")
             return
 
@@ -47,24 +45,25 @@ class BakeBeamBoxMap(component):
 
             # Ensure layer exists and clear it if specified
             self.ensure_layer_exists(layer_name)
-            if clear_layer:
+            if clear_layer_content:
                 self.delete_objects_on_layer(layer_name)
 
-            frames = [frame_to_rhino(b.frame) for b in model.beams]
-            breps = [beam.geometry.native_brep for beam in model.beams]
+            for model in model_list:
+                if hasattr(model, "beams"):
+                    frames = [frame_to_rhino(b.frame) for b in model.beams]
+                    breps = [beam.geometry.native_brep for beam in model.beams]
 
-            if frames and breps:
-                rs.EnableRedraw(False)
-                for brep, frame in zip(breps, frames):
-                    
-                    # Add brep to the document
-                    guid = sc.doc.Objects.Add(brep)
-                    if layer_name:
-                        rs.ObjectLayer(guid, layer_name)
-                    
-                    # Create box mapping and apply it
-                    boxmap = self.create_box_map(frame, dimx, dimy, dimz)
-                    sc.doc.Objects.ModifyTextureMapping(guid, 1, boxmap)
+                    if frames and breps:
+                        rs.EnableRedraw(False)
+                        for brep, frame in zip(breps, frames):
+                            # Add brep to the document
+                            guid = sc.doc.Objects.Add(brep)
+                            if layer_name:
+                                rs.ObjectLayer(guid, layer_name)
+
+                            # Create box mapping and apply it
+                            boxmap = self.create_box_map(frame, dimx, dimy, dimz)
+                            sc.doc.Objects.ModifyTextureMapping(guid, 1, boxmap)
         finally:
             # Restore document context back to Grasshopper
             sc.doc = ghdoc
@@ -81,7 +80,7 @@ class BakeBeamBoxMap(component):
         w = pln.ZAxis
         pt = pln.Origin
 
-        # random deviation
+        # Random deviation
         a = math.pi * 0.5
         randangle = (random.random() - 0.5) * a
         v.Rotate(randangle, pln.XAxis)
@@ -93,15 +92,15 @@ class BakeBeamBoxMap(component):
         randpos = sx * random.random()
         pt += pln.XAxis * randpos
 
-        # create box mapping
-        mappingPln = Plane(pt, w, v)
+        # Create box mapping
+        mapping_pln = Plane(pt, w, v)
         dx = Interval(-sx * 0.5, sx * 0.5)
         dy = Interval(-sy * 0.5, sy * 0.5)
         dz = Interval(-sz * 0.5, sz * 0.5)
 
-        BoxMap = Render.TextureMapping.CreateBoxMapping(mappingPln, dx, dy, dz, False)
+        box_map = Render.TextureMapping.CreateBoxMapping(mapping_pln, dx, dy, dz, False)
 
-        return BoxMap
+        return box_map
 
     @staticmethod
     def ensure_layer_exists(layer_name):
