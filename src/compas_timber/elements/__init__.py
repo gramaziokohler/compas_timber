c2d9a1cabc706f3fc670b3c447ee7adcdf5ad844
obucklin <obucklin@gmail.com>
2024-11-12 13:39:43 +0100

    slimmed to minimum

--- src/compas_timber/elements/__init__.py
+++ src/compas_timber/elements/__init__.py
@@ -1,6 +1,7 @@
 from .beam import Beam
 from .plate import Plate
 from .wall import Wall
+from .fastener import Fastener
 from .features import BrepSubtraction
 from .features import CutFeature
 from .features import DrillFeature
@@ -11,6 +12,7 @@ __all__ = [
     "Wall",
     "Beam",
     "Plate",
+    "Fastener",
     "CutFeature",
     "DrillFeature",
     "MillVolume",
--- tests/compas_timber/test_fastener.py
+++ tests/compas_timber/test_fastener.py
@@ -1,58 +1,25 @@
-import pytest
-from compas_timber.elements.fasteners.fastener import Fastener
-from compas_timber.elements import Beam
-from compas.geometry import Frame
+from compas_timber.elements import Fastener
 
 
-@pytest.fixture
-def mock_elements():
-    beam_1 = Beam(Frame([0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]), 100.0, 10.0, 20.0)
-    beam_2 = Beam(Frame([0.0, 0.0, 0.0], [0.0, 1.0, 0.0], [-1.0, 0.0, 0.0]), 100.0, 10.0, 20.0)
-    return [beam_1, beam_2]
-
-
-def test_fastener_initialization(mock_elements):
-    fastener = Fastener(mock_elements)
-    assert fastener.elements == mock_elements
-    assert fastener.features == []
+def test_fastener_initialization():
+    fastener = Fastener()
     assert fastener.attributes == {}
     assert fastener.debug_info == []
 
 
-def test_fastener_repr(mock_elements):
-    fastener = Fastener(mock_elements)
-    assert repr(fastener) == "Fastener(Beam None, Beam None)"
+def test_fastener_repr():
+    fastener = Fastener()
+    assert (
+        repr(fastener)
+        == "Fastener(frame=Frame(point=Point(x=0.0, y=0.0, z=0.0), xaxis=Vector(x=1.0, y=0.0, z=0.0), yaxis=Vector(x=0.0, y=1.0, z=0.0)), name=Fastener)"
+    )
 
 
-def test_fastener_str(mock_elements):
-    fastener = Fastener(mock_elements)
-    assert str(fastener) == "Fastener connecting Beam None, Beam None"
+def test_fastener_str():
+    fastener = Fastener()
+    assert str(fastener) == "<Fastener Fastener>"
 
 
-def test_fastener_is_fastener(mock_elements):
-    fastener = Fastener(mock_elements)
+def test_fastener_is_fastener():
+    fastener = Fastener()
     assert fastener.is_fastener is True
-
-
-def test_fastener_add_features(mock_elements):
-    fastener = Fastener(mock_elements)
-    feature = "feature1"
-    fastener.add_features(feature)
-    assert fastener.features == [feature]
-
-
-def test_fastener_remove_features(mock_elements):
-    fastener = Fastener(mock_elements)
-    feature = "feature1"
-    fastener.add_features(feature)
-    fastener.remove_features(feature)
-    assert fastener.features == []
-
-
-def test_fastener_remove_all_features(mock_elements):
-    fastener = Fastener(mock_elements)
-    feature1 = "feature1"
-    feature2 = "feature2"
-    fastener.add_features([feature1, feature2])
-    fastener.remove_features()
-    assert fastener.features == []
