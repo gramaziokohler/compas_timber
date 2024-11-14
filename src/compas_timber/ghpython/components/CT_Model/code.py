604da9192fa8d7b495e376133a37e891f92f5480
oliver bucklin <bucklin@arch.ethz.ch>
2024-11-14 10:00:34 +0100

    format

--- src/compas_timber/ghpython/components/CT_Model/code.py
+++ src/compas_timber/ghpython/components/CT_Model/code.py
@@ -44,7 +44,9 @@ class ModelComponent(component):
 
         if unmatched_pairs:
             for pair in unmatched_pairs:
-                self.addRuntimeMessage(Warning, "No joint rule found for beams {} and {}".format(pair[0].key, pair[1].key)) #TODO: add to debug_info
+                self.addRuntimeMessage(
+                    Warning, "No joint rule found for beams {} and {}".format(pair[0].key, pair[1].key)
+                )  # TODO: add to debug_info
 
         if joints:
             # apply reversed. later joints in orginal list override ealier ones
