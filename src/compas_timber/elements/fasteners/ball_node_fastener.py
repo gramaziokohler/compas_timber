a45e6d7eeea7720ccc86108af55d86c88074e52b
obucklin <obucklin@gmail.com>
2024-11-19 16:29:33 +0100

    clean up BNF

--- src/compas_timber/elements/fasteners/ball_node_fastener.py
+++ src/compas_timber/elements/fasteners/ball_node_fastener.py
@@ -11,8 +11,7 @@ from compas.geometry.intersections import intersection_sphere_line
 from compas_timber.elements import DrillFeature
 from compas_timber.elements import BrepSubtraction
 from compas_timber.elements import CutFeature
-from compas_timber.elements.fasteners.fastener import Fastener
-
+from compas_timber.elements.fastener import Fastener
 
 
 class BallNodeFastener(Fastener):
@@ -36,17 +35,14 @@ class BallNodeFastener(Fastener):
     @property
     def __data__(self):
         data = super(Fastener, self).__data__
-
         return data
 
     def __init__(self, geometry = None, **kwargs):
         super(BallNodeFastener, self).__init__(**kwargs)
         self.geometry = geometry
-        self.features = []
         self.attributes = {}
         self.attributes.update(kwargs)
         self.debug_info = []
-        self.test = []
 
     def __repr__(self):
         # type: () -> str
@@ -65,7 +61,6 @@ class BallNodeFastener(Fastener):
     def shape(self):
         # type: () -> Brep
         return self.geometry
-
 
     @property
     def key(self):
@@ -87,51 +82,6 @@ class BallNodeFastener(Fastener):
     def geometry(self, geometry):
         self._geometry = geometry
 
-    def compute_geometry(self):
-        # type: (bool) -> compas.geometry.Brep
-        """Compute the geometry of the fastener.
-
-        Returns
-        -------
-        :class:`compas.geometry.Brep`
-
-        """
-        return self.shape
-
-    def compute_aabb(self, inflate=0.0):
-        # type: (float) -> compas.geometry.Box
-        """Computes the Axis Aligned Bounding Box (AABB) of the element.
-
-        Parameters
-        ----------
-        inflate : float, optional
-            Offset of box to avoid floating point errors.
-
-        Returns
-        -------
-        :class:`~compas.geometry.Box`
-            The AABB of the element.
-
-        """
-        raise NotImplementedError
-
-    def compute_obb(self, inflate=0.0):
-        # type: (float | None) -> compas.geometry.Box
-        """Computes the Oriented Bounding Box (OBB) of the element.
-
-        Parameters
-        ----------
-        inflate : float
-            Offset of box to avoid floating point errors.
-
-        Returns
-        -------
-        :class:`compas.geometry.Box`
-            The OBB of the element.
-
-        """
-        raise NotImplementedError
-
     def compute_collision_mesh(self):
         # type: () -> compas.datastructures.Mesh
         """Computes the collision geometry of the element.
@@ -143,45 +93,3 @@ class BallNodeFastener(Fastener):
 
         """
         return self.shape.to_mesh()
-
-    # ==========================================================================
-    # Alternative constructors
-    # ==========================================================================
-
-
-    # ==========================================================================
-    # Features
-    # ==========================================================================
-
-    @reset_computed
-    def add_features(self, features):
-        # type: (Feature | list[Feature]) -> None
-        """Adds one or more features to the fastener.
-
-        Parameters
-        ----------
-        features : :class:`~compas_timber.parts.Feature` | list(:class:`~compas_timber.parts.Feature`)
-            The feature to be added.
-
-        """
-        if not isinstance(features, list):
-            features = [features]
-        self.features.extend(features)  # type: ignore
-
-    @reset_computed
-    def remove_features(self, features=None):
-        # type: (None | Feature | list[Feature]) -> None
-        """Removes a feature from the fastener.
-
-        Parameters
-        ----------
-        feature : :class:`~compas_timber.parts.Feature` | list(:class:`~compas_timber.parts.Feature`)
-            The feature to be removed. If None, all features will be removed.
-
-        """
-        if features is None:
-            self.features = []
-        else:
-            if not isinstance(features, list):
-                features = [features]
-            self.features = [f for f in self.features if f not in features]
