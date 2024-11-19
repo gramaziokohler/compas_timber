7b54d5ba497ea17dc6d28ca534f13b7b5e53eb80
obucklin <obucklin@gmail.com>
2024-11-19 13:40:15 +0100

    added test case

--- tests/compas_timber/test_t_butt_plate_joint.py
+++ tests/compas_timber/test_t_butt_plate_joint.py
@@ -1,5 +1,6 @@
 from compas.geometry import Point
 from compas.geometry import Vector
+from compas.geometry import Frame
 
 from compas_timber.connections import TButtPlateJoint
 from compas_timber.elements import Beam
@@ -14,8 +15,12 @@ def test_create():
     A.add_element(B2)
     instance = TButtPlateJoint.create(A, B1, B2)
 
+    F1 = Frame(point=Point(x=0.0, y=0.0, z=0.05), xaxis=Vector(x=1.0, y=0.0, z=0.0), yaxis=Vector(x=-0.0, y=1.0, z=0.0))
+    F2 = Frame(point=Point(x=0.0, y=0.0, z=-0.05), xaxis=Vector(x=1.0, y=0.0, z=0.0), yaxis=Vector(x=0.0, y=-1.0, z=0.0))
+
     assert len(instance.beams) == 2
     assert isinstance(instance, TButtPlateJoint)
     assert instance.main_beam == B1
     assert instance.cross_beam == B2
     assert len(instance.elements) == 4
+    assert F1, F2 == [fast.frame for fast in instance.fasteners]
