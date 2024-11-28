4cb8f4a9c7fa44aeee363fead036ac0a78fb8039
obucklin <obucklin@gmail.com>
2024-11-28 18:13:51 +0100

    format lint test

--- src/compas_timber/ghpython/components/CT_Fastener_Beam_Interface/code.py
+++ src/compas_timber/ghpython/components/CT_Fastener_Beam_Interface/code.py
@@ -1,12 +1,13 @@
-from ghpythonlib.componentbase import executingcomponent as component
-from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
-from compas_timber.design import FeatureDefinition
-from compas_timber.elements import FastenerTimberInterface
 from compas.geometry import Brep
 from compas.scene import Scene
+from ghpythonlib.componentbase import executingcomponent as component
+from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
+
+from compas_timber.design import FeatureDefinition
+from compas_timber.elements import FastenerTimberInterface
+
 
 class MyComponent(component):
-
     def RunScript(self, outline_points, thickness, drill_points, drill_diameters, extra_shapes, features):
         holes = []
         if drill_points and drill_diameters:
@@ -21,7 +22,13 @@ class MyComponent(component):
 
         features = [feature.feature if isinstance(feature, FeatureDefinition) else feature for feature in features]
 
-        fast_int = FastenerTimberInterface(outline_points, thickness, holes, shapes = [Brep.from_native(brep) for brep in extra_shapes], features = features)
+        fast_int = FastenerTimberInterface(
+            outline_points,
+            thickness,
+            holes,
+            shapes=[Brep.from_native(brep) for brep in extra_shapes],
+            features=features,
+        )
 
         shape = None
         if outline_points:
