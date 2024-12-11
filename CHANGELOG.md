bd3fedf3b28464e2288ccfae6a86eb399ba78904
obucklin <obucklin@gmail.com>
2024-12-11 17:53:51 +0100

    merge new bug fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -53,6 +53,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Added `CT_Beams_From_Mesh` GH component.
 * Added new `compas_timber._fabrication.FrenchRidgeLap`.
 * Added new `compas_timber.connections.LFrenchRidgeLapJoint`.
+* Added `create` override to `BallNodeJoint`.
 
 ### Changed
 
@@ -72,6 +73,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Changed `beam` objects get added to `Joint.elements` in `Joint.create()`.
 * Fixed bug in vizualization of tenon/mortise in `compas_timber._fabrication.StepJoint`and `compas_timber._fabrication.StepJointNotch`.
 * Changed `model.process_joinery()`so that it calls `joint.check_elements_compatibility()` before adding extensions and features.
+* Fixed `JointRuleFromList` GH component.
 
 ### Removed
 
--- src/compas_timber/design/workflow.py
+++ src/compas_timber/design/workflow.py
@@ -242,36 +242,36 @@ class JointDefinition(object):
 
     """
 
-    def __init__(self, joint_type, beams, **kwargs):
+    def __init__(self, joint_type, elements, **kwargs):
         # if not issubclass(joint_type, Joint):
         #     raise UserWarning("{} is not a valid Joint type!".format(joint_type.__name__))
-        if len(beams) < 2:
-            raise UserWarning("Joint requires at least two Beams, got {}.".format(len(beams)))
+        if len(elements) < 2:
+            raise UserWarning("Joint requires at least two Beams, got {}.".format(len(elements)))
 
         self.joint_type = joint_type
-        self.beams = beams
+        self.elements = elements
         self.kwargs = kwargs
 
     def __repr__(self):
-        return "{}({}, {}, {})".format(JointDefinition.__name__, self.joint_type.__name__, self.beams, self.kwargs)
+        return "{}({}, {}, {})".format(JointDefinition.__name__, self.joint_type.__name__, self.elements, self.kwargs)
 
     def ToString(self):
         return repr(self)
 
     def __hash__(self):
-        return hash((self.joint_type, self.beams))
+        return hash((self.joint_type, self.elements))
 
     def is_identical(self, other):
         return (
             isinstance(other, JointDefinition)
             and self.joint_type == other.joint_type
-            and set([b.key for b in self.beams]) == set([b.key for b in other.beams])
+            and set([e.key for e in self.elements]) == set([e.key for e in other.elements])
         )
 
     def match(self, beams):
         """Returns True if beams are defined within this JointDefinition."""
-        set_a = set([id(b) for b in beams])
-        set_b = set([id(b) for b in self.beams])
+        set_a = set([id(e) for e in beams])
+        set_b = set([id(e) for e in self.elements])
         return set_a == set_b
 
 
