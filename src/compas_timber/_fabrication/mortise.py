694958da2251abad4d621fc6d68e3d885d38d97a
panos ppchr <ppapachar@gmail.com>
2024-12-16 15:39:25 +0100

    add tolerance to the bool subtr

--- src/compas_timber/_fabrication/mortise.py
+++ src/compas_timber/_fabrication/mortise.py
@@ -442,6 +442,7 @@ class Mortise(BTLxProcess):
                     geometry,
                     "Failed to fillet the edges of the tenon volume based on the shape: {}".format(str(e)),
                 )
+
         # remove tenon volume to geometry
         try:
             return geometry - mortise_volume
@@ -499,13 +500,14 @@ class Mortise(BTLxProcess):
         assert self.width is not None
         assert self.depth is not None
 
+        TOL.RELATIVE = 1e-3
         cutting_frame = self.frame_from_params_and_beam(beam)
         # translate the cutting frame to the center of the mortise
-        translation_vector = (-cutting_frame.normal * (self.depth-TOL.RELATIVE) - cutting_frame.yaxis * self.length)
+        translation_vector = (-cutting_frame.normal * (self.depth+TOL.RELATIVE) - cutting_frame.yaxis * self.length)
         cutting_frame.translate(translation_vector * 0.5)
 
         # get the tenon as a box
-        tenon_box = Box(self.width, self.length, self.depth, cutting_frame)
+        tenon_box = Box(self.width, self.length, self.depth+TOL.RELATIVE, cutting_frame)
         return Brep.from_box(tenon_box)
 
 
