ea90837257490def5fd98c920fadd2cd217c09f2
panos ppchr <ppapachar@gmail.com>
2024-12-19 11:48:19 +0100

    Refactor joint topology checks to support multiple topologies

--- src/compas_timber/ghpython/components/CT_Joint_Rule_Topology_X/code.py
+++ src/compas_timber/ghpython/components/CT_Joint_Rule_Topology_X/code.py
@@ -17,8 +17,12 @@ class X_TopologyJointRule(component):
         super(X_TopologyJointRule, self).__init__()
         self.classes = {}
         for cls in get_leaf_subclasses(Joint):
-            if cls.SUPPORTED_TOPOLOGY == JointTopology.TOPO_X:
-                self.classes[cls.__name__] = cls
+            if isinstance(cls.SUPPORTED_TOPOLOGY, list):
+                if JointTopology.TOPO_X in cls.SUPPORTED_TOPOLOGY:
+                    self.classes[cls.__name__] = cls
+            else:
+                if cls.SUPPORTED_TOPOLOGY == JointTopology.TOPO_X:
+                    self.classes[cls.__name__] = cls
         if ghenv.Component.Params.Output[0].NickName == "Rule":
             self.joint_type = XHalfLapJoint
             self.clicked = False
@@ -37,7 +41,10 @@ class X_TopologyJointRule(component):
             for i, val in enumerate(args):
                 if val is not None:
                     kwargs[self.arg_names()[i]] = val
-            if self.joint_type.SUPPORTED_TOPOLOGY != JointTopology.TOPO_X:
+            supported_topo = self.joint_type.SUPPORTED_TOPOLOGY
+            if not isinstance(supported_topo, list):
+                supported_topo = [supported_topo]
+            if JointTopology.TOPO_X not in supported_topo:
                 self.AddRuntimeMessage(Warning, "Joint type does not match topology. Joint may not be generated.")
             return TopologyRule(JointTopology.TOPO_X, self.joint_type, **kwargs)
 
