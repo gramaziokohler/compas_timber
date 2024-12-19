c7f59f66f3f6d731172622355c899c66b87b003f
panos ppchr <ppapachar@gmail.com>
2024-12-19 13:15:23 +0100

    cross beam extension for L-TOPO

--- src/compas_timber/connections/t_tenon_mortise.py
+++ src/compas_timber/connections/t_tenon_mortise.py
@@ -181,13 +181,27 @@ class TenonMortiseJoint(Joint):
 
         """
         assert self.main_beam and self.cross_beam
+        extension_tolerance = 0.01  # TODO: this should be proportional to the unit used
+
+        #cross_beam
+        try:
+            opposing_ref_side = self.main_beam.opposing_side_index(self.main_beam_ref_side_index)
+            cutting_plane = self.main_beam.ref_sides[opposing_ref_side]
+            start_cross, end_cross = self.cross_beam.extension_to_plane(cutting_plane)
+        except AttributeError as ae:
+            raise BeamJoinningError(beams=self.elements, joint=self, debug_info=str(ae), debug_geometries=[cutting_plane])
+        self.cross_beam.add_blank_extension(
+            start_cross + extension_tolerance,
+            end_cross + extension_tolerance,
+            self.guid
+            )
+        #main_beam
         try:
             cutting_plane = self.cross_beam.ref_sides[self.cross_beam_ref_side_index]
             cutting_plane.translate(-cutting_plane.normal * self.height)
             start_main, end_main = self.main_beam.extension_to_plane(cutting_plane)
         except AttributeError as ae:
             raise BeamJoinningError(beams=self.elements, joint=self, debug_info=str(ae), debug_geometries=[cutting_plane])
-        extension_tolerance = 0.01  # TODO: this should be proportional to the unit used
         self.main_beam.add_blank_extension(
             start_main + extension_tolerance,
             end_main + extension_tolerance,
