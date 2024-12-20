1a0afa19ca70803d14516a1642e5a4ece797afd6
panos ppchr <ppapachar@gmail.com>
2024-12-20 16:20:44 +0100

    add btlx test file

--- src/compas_timber/fabrication/__init__.py
+++ src/compas_timber/fabrication/__init__.py
@@ -2,16 +2,8 @@ from .btlx import BTLx
 from .btlx import BTLxProcess
 from .btlx import BTLxPart
 
-from .btlx_processes.btlx_jack_cut import BTLxJackCut
-from .btlx_processes.btlx_lap import BTLxLap
-from .joint_factories.l_miter_factory import LMiterFactory
-
 __all__ = [
     "BTLx",
     "BTLxPart",
     "BTLxProcess",
-    "BTLxJackCut",
-    "BTLxLap",
-    "BTLxFrenchRidgeLap",
-    "LMiterFactory",
 ]
