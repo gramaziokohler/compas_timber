49bbf543569b4930421ce0e64f9ff85db05ef6f0
panos ppchr <ppapachar@gmail.com>
2025-01-10 10:40:12 +0100

    remove param classes from second level import

--- src/compas_timber/fabrication/__init__.py
+++ src/compas_timber/fabrication/__init__.py
@@ -3,29 +3,17 @@ from .btlx import BTLxProcessing
 from .btlx import BTLxPart
 from .btlx import OrientationType
 from .jack_cut import JackRafterCut
-from .jack_cut import JackRafterCutParams
 from .double_cut import DoubleCut
-from .double_cut import DoubleCutParams
 from .drilling import Drilling
-from .drilling import DrillingParams
 from .step_joint_notch import StepJointNotch
-from .step_joint_notch import StepJointNotchParams
 from .step_joint import StepJoint
-from .step_joint import StepJointParams
 from .dovetail_tenon import DovetailTenon
-from .dovetail_tenon import DovetailTenonParams
 from .dovetail_mortise import DovetailMortise
-from .dovetail_mortise import DovetailMortiseParams
 from .lap import Lap
-from .lap import LapParams
 from .french_ridge_lap import FrenchRidgeLap
-from .french_ridge_lap import FrenchRidgeLapParams
 from .tenon import Tenon
-from .tenon import TenonParams
 from .mortise import Mortise
-from .mortise import MortiseParams
 from .slot import Slot
-from .slot import SlotParams
 from .btlx import TenonShapeType
 from .btlx import EdgePositionType
 from .btlx import LimitationTopType
@@ -38,29 +26,17 @@ __all__ = [
     "BTLxProcessing",
     "JackRafterCut",
     "OrientationType",
-    "JackRafterCutParams",
     "DoubleCut",
-    "DoubleCutParams",
     "Drilling",
-    "DrillingParams",
     "StepJointNotch",
-    "StepJointNotchParams",
     "StepJoint",
-    "StepJointParams",
     "DovetailTenon",
-    "DovetailTenonParams",
     "DovetailMortise",
-    "DovetailMortiseParams",
     "Lap",
-    "LapParams",
     "FrenchRidgeLap",
-    "FrenchRidgeLapParams",
     "Tenon",
-    "TenonParams",
     "Mortise",
-    "MortiseParams",
     "Slot",
-    "SlotParams",
     "TenonShapeType",
     "EdgePositionType",
     "LimitationTopType",
