9d15ab6c1fdb43e37ad9d0701b5f848c512bbd1d
obucklin <obucklin@gmail.com>
2025-01-27 18:22:56 +0100

    fixed var names

--- src/compas_timber/connections/l_miter.py
+++ src/compas_timber/connections/l_miter.py
@@ -148,5 +148,5 @@ class LMiterJoint(Joint):
 
     def restore_beams_from_keys(self, model):
         """After de-serialization, restores references to the main and cross beams saved in the model."""
-        self.main_beam = model.element_by_guid(self.main_beam_guid)
-        self.cross_beam = model.element_by_guid(self.cross_beam_guid)
+        self.beam_a = model.element_by_guid(self.beam_a_guid)
+        self.beam_b = model.element_by_guid(self.beam_b_guid)
