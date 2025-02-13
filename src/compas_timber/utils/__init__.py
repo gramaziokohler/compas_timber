77ae096358e499acea3a0d7174677ca32de9bdc4
Chen Kasirer <ckasirer@ethz.ch>
2025-02-13 14:43:25 +0100

    comment

--- src/compas_timber/utils/__init__.py
+++ src/compas_timber/utils/__init__.py
@@ -194,7 +194,8 @@ def distance_segment_segment(segment_a, segment_b):
     if not pta:  # segments are parallel
         dists = []
         for pair in product(segment_a, segment_b):
-            # gets shortest distance between all 4 possible pairs of endpoints. only for L/I_Topo. T_Topology cannot have paralell segments"""
+            # gets shortest distance between all 4 possible pairs of endpoints. only for L/I_Topo.
+            # T_Topology cannot have paralell segments
             dists.append(distance_point_point(*pair))
         return min(dists)
     pt_seg_a = closest_point_on_segment(pta, segment_a)
