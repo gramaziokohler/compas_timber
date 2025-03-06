9c4726c28413912ec6417aa4513c75c06c8bdbcf
obucklin <obucklin@gmail.com>
2025-03-06 18:07:00 +0100

    fix component args

--- src/compas_timber/ghpython/components/CT_Label_Beams/code.py
+++ src/compas_timber/ghpython/components/CT_Label_Beams/code.py
@@ -4,10 +4,12 @@ from compas_timber.planning import DeferredLabel
 
 
 class LabelBeamsComponent(component):
-    def RunScript(self, elements, base_string=None, char_to_replace=None, attributes=None, text_height=None, ref_side_index=0):
+    def RunScript(self, elements, base_string, char_to_replace, attributes, text_height, ref_side_index):
         f_defs = []
         if not elements:
             return
+        if ref_side_index is None:
+            ref_side_index = 0
         if len(list(elements)) == len(base_string):
             for st, el in zip(base_string, elements):
                 f_defs.append(DeferredLabel([el], text=st, text_height=text_height, ref_side_index=ref_side_index))
