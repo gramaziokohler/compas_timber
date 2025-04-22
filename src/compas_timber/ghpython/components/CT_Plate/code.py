937be2d4ede3088bcefef31e7ca6ed07299cc310
Chen Kasirer <ckasirer@ethz.ch>
2025-04-22 14:01:06 +0200

    some changes in component

--- src/compas_timber/ghpython/components/CT_Plate/code.py
+++ src/compas_timber/ghpython/components/CT_Plate/code.py
@@ -1,28 +1,33 @@
 """Creates a Beam from a LineCurve."""
 
+import Rhino
 import rhinoscriptsyntax as rs
 from compas.scene import Scene
-from compas_rhino.conversions import curve_to_compas
+from compas_rhino.conversions import curve_to_compas_polyline
 from compas_rhino.conversions import vector_to_compas
 from ghpythonlib.componentbase import executingcomponent as component
+from Grasshopper.Kernel.GH_RuntimeMessageLevel import Warning
 from Rhino.RhinoDoc import ActiveDoc
 
-from compas_timber.elements import Plate as CTPlate
+from compas_timber.elements import Plate
 from compas_timber.ghpython.ghcomponent_helpers import list_input_valid
 from compas_timber.ghpython.rhino_object_name_attributes import update_rhobj_attributes_name
 
 
-class Plate(component):
-    def RunScript(self, outline, outline_b, thickness, vector, category, updateRefObj):
-        if not list_input_valid(self, outline, outline):
+class PlateComponent(component):
+    def RunScript(self, outline_a, outline_b, thickness, vector, category, updateRefObj):
+        # Accepted combinations: outline_a + outline_b OR outline_a + thickness + vector
+        if not list_input_valid(self, outline_a, "OutlineTop"):
             return
         if not thickness and not outline_b:
-            self.AddRuntimeMessage(Warning, "Input required on either 'Thickness' or 'outline_b")
+            self.AddRuntimeMessage(Warning, "Input required on either 'Thickness' or 'OutlineBottom")
             return
         if not outline_b:
             outline_b = [None]
+        if not thickness:
+            thickness = [0.0]
         if not vector:
-            vector = [None]
+            vector = [-Rhino.Geometry.Vector3f.ZAxis]
         if not category:
             category = [None]
 
@@ -30,15 +35,15 @@ class Plate(component):
         scene = Scene()
 
         # check list lengths for consistency
-        N = len(outline)
+        N = len(outline_a)
         if len(thickness) not in (0, 1, N):
-            print(" In 'T' I need either one or the same number of inputs as the Crv parameter.")
+            self.AddRuntimeMessage(Warning, " In 'T' I need either one or the same number of inputs as the Crv parameter.")
         if len(outline_b) not in (0, N):
-            print(" In 'outline_b' I need either one or the same number of inputs as the Crv parameter.")
+            self.AddRuntimeMessage(Warning, " In 'OutlineTop' I need either one or the same number of inputs as the Crv parameter.")
         if len(category) not in (0, 1, N):
-            print(" In 'Category' I need either none, one or the same number of inputs as the Crv parameter.")
+            self.AddRuntimeMessage(Warning, " In 'Category' I need either none, one or the same number of inputs as the Crv parameter.")
         if len(vector) not in (0, 1, N):
-            print(" In 'Vector' I need either none, one or the same number of inputs as the Crv parameter.")
+            self.AddRuntimeMessage(Warning, " In 'Vector' I need either none, one or the same number of inputs as the Crv parameter.")
 
         # duplicate data if None or single value
         if len(outline_b) != N:
@@ -50,23 +55,22 @@ class Plate(component):
         if len(category) != N:
             category = [category[0] for _ in range(N)]
 
-        for line_a, line_b, t, v, c in zip(outline, outline_b, thickness, vector, category):
+        for line_a, line_b, t, v, c in zip(outline_a, outline_b, thickness, vector, category):
             guid_a, geometry_a = self._get_guid_and_geometry(line_a)
             rhino_polyline_a = rs.coercecurve(geometry_a)
-            line_a = curve_to_compas(rhino_polyline_a)
+            line_a = curve_to_compas_polyline(rhino_polyline_a)
 
             if line_b:
                 guid_b, geometry_b = self._get_guid_and_geometry(line_b)
                 rhino_polyline_b = rs.coercecurve(geometry_b)
-                line_b = curve_to_compas(rhino_polyline_b)
-                plate = CTPlate(line_a.points, line_b.points)
+                line_b = curve_to_compas_polyline(rhino_polyline_b)
+                plate = Plate(line_a.points, line_b.points)
                 plate.attributes["rhino_guid_a"] = str(guid_a) if guid_a else None
                 if updateRefObj and guid_b:
                     update_rhobj_attributes_name(guid_b, "outline", str(line_b))
 
             else:
-                print(v)
-                plate = CTPlate.from_outline_thickness(line_a.points, t, vector_to_compas(v) if v else None)
+                plate = Plate.from_outline_thickness(line_a.points, t, vector_to_compas(v) if v else None)
 
             plate.attributes["rhino_guid_a"] = str(guid_a) if guid_a else None
             plate.attributes["category"] = c
