59aa3d7af44a15097e46d7b0fa9938efda9911ee
Chen Kasirer <ckasirer@ethz.ch>
2025-05-07 18:39:35 +0200

    added tasks to update minimum version in components

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+* Added tasks `update-gh-header` to update the version in the header of the GH components.
+
 ### Changed
 
 ### Removed
--- tasks.py
+++ tasks.py
@@ -1,13 +1,53 @@
 from __future__ import print_function
 
 import os
+from pathlib import Path
 
 from compas_invocations2 import build
 from compas_invocations2 import docs
 from compas_invocations2 import style
 from compas_invocations2 import tests
 from compas_invocations2 import grasshopper
+
 from invoke.collection import Collection
+from invoke.tasks import task
+from invoke.exceptions import Exit
+import tomlkit
+
+
+def _get_version_from_toml(toml_file: str) -> str:
+    with open(toml_file, "r") as f:
+        pyproject_data = tomlkit.load(f)
+    if not pyproject_data:
+        raise Exit("Failed to load pyproject.toml.")
+
+    version = pyproject_data.get("tool", {}).get("bumpversion", {}).get("current_version", None)
+    if not version:
+        raise Exit("Failed to get version from pyproject.toml. Please provide a version number.")
+    return version
+
+
+@task(help={"version": "New minimum version to set in the header. If not provided, current version is used."})
+def update_gh_header(ctx, version=None):
+    """Update the minimum version header of all CPython Grasshopper components."""
+    version = version or _get_version_from_toml("pyproject.toml")
+
+    new_header = f"#r: compas_timber>={version}"
+
+    for file in Path(ctx.ghuser_cpython.source_dir).glob("**/code.py"):
+        try:
+            with open(file, "r", encoding="utf-8") as f:
+                original_content = f.readlines()
+            with open(file, "w", encoding="utf-8") as f:
+                for line in original_content:
+                    if line.startswith("# r: compas_timber"):
+                        f.write(new_header + "\n")
+                    else:
+                        f.write(line)
+            print(f"✅ Updated: {file}")
+        except Exception as e:
+            print(f"❌ Failed to update {file}: {e}")
+
 
 ns = Collection(
     docs.help,
@@ -26,6 +66,7 @@ ns = Collection(
     build.build_cpython_ghuser_components,
     grasshopper.yakerize,
     grasshopper.publish_yak,
+    update_gh_header,
 )
 
 ns.configure(
