c9bf56e429e264c0b9b9c3658cb6ef30d717c796
paulocinco <jaeggi.paul@gmail.com>
2025-11-24 16:27:44 +0100

    added simplescraf

--- src/compas_timber/connections/__init__.py
+++ src/compas_timber/connections/__init__.py
@@ -1,5 +1,6 @@
 from .joint import Joint
 from .butt_joint import ButtJoint
+from .i_simple_scarf import ISimpleScarf
 from .l_butt import LButtJoint
 from .l_lap import LLapJoint
 from .l_miter import LMiterJoint
