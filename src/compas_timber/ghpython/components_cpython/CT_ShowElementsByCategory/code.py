a2703f40b1325f68afba8c750aaadd727d9362d4
Nastia <astryzhevska@ethz.ch>
2025-12-09 17:22:36 +0100

    iterate through elements not beams, do not rename output

--- src/compas_timber/ghpython/components_cpython/CT_ShowElementsByCategory/code.py
+++ src/compas_timber/ghpython/components_cpython/CT_ShowElementsByCategory/code.py
@@ -10,26 +10,25 @@ from compas_timber.ghpython.ghcomponent_helpers import rename_gh_output
 class ShowElementsByCategory(Grasshopper.Kernel.GH_ScriptInstance):
     def __init__(self):
         super(ShowElementsByCategory, self).__init__()
-        self.element_type = None
-        if ghenv.Component.Params.Output[0].NickName == "elements":
-            self.joint_type = None
-        else:
-            self.element_type = ghenv.Component.Params.Output[0].NickName
+        ghenv.Component.Params.Output[0].NickName == "elements"
+        self.element_type = ghenv.Component.Message if ghenv.Component.Message else None
 
     def RunScript(self, model):
         self.names = set(element.attributes.get("category") for element in model.elements())
         self.names.discard(None)
+
         if not self.element_type:
             ghenv.Component.Message = "Select element type from context menu (right click)"
             ghenv.Component.AddRuntimeMessage(Grasshopper.Kernel.GH_RuntimeMessageLevel.Warning, "Select element type from context menu (right click)")
             return None
-        else:
-            ghenv.Component.Message = self.element_type
-            scene = Scene()
-            for beam in model.beams:
-                if beam.attributes["category"] == self.element_type:
-                    scene.add(beam.geometry)
-            return scene.draw()
+        
+        ghenv.Component.Message = self.element_type
+        
+        scene = Scene()
+        for element in model.elements():
+            if element.attributes.get("category") == self.element_type:
+                scene.add(element.geometry)
+        return scene.draw()
 
     def AppendAdditionalMenuItems(self, menu):
         menu.Items.Add(ToolStripSeparator())
@@ -39,6 +38,5 @@ class ShowElementsByCategory(Grasshopper.Kernel.GH_ScriptInstance):
                 item.Checked = True
 
     def on_item_click(self, sender, event_info):
-        self.element_type = str(sender)
-        rename_gh_output(self.element_type, 0, ghenv)
+        self.element_type = sender.Text
         ghenv.Component.ExpireSolution(True)
