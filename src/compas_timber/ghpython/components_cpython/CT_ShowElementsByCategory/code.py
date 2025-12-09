512e5b594140b601ee40bc7c0277b33bcc02e346
Nastia <astryzhevska@ethz.ch>
2025-12-09 16:28:51 +0100

    no error if no category

--- src/compas_timber/ghpython/components_cpython/CT_ShowElementsByCategory/code.py
+++ src/compas_timber/ghpython/components_cpython/CT_ShowElementsByCategory/code.py
@@ -18,7 +18,7 @@ class ShowElementsByCategory(Grasshopper.Kernel.GH_ScriptInstance):
 
     def RunScript(self, model):
         self.names = set(element.attributes.get("category") for element in model.elements())
-        self.names.remove(None)
+        self.names.discard(None)
         if not self.element_type:
             ghenv.Component.Message = "Select element type from context menu (right click)"
             ghenv.Component.AddRuntimeMessage(Grasshopper.Kernel.GH_RuntimeMessageLevel.Warning, "Select element type from context menu (right click)")
@@ -32,8 +32,7 @@ class ShowElementsByCategory(Grasshopper.Kernel.GH_ScriptInstance):
             return scene.draw()
 
     def AppendAdditionalMenuItems(self, menu):
-        if not self.RuntimeMessages(Warning):
-            menu.Items.Add(ToolStripSeparator())
+        menu.Items.Add(ToolStripSeparator())
         for name in self.names:
             item = menu.Items.Add(name, None, self.on_item_click)
             if self.element_type and name == self.element_type:
