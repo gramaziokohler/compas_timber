4536e560be71a758c38d9b06c586ef1e79d55012
oliver bucklin <bucklin@arch.ethz.ch>
2026-01-16 09:06:56 +0100

    start wall module

---
+++ src/compas_timber/elements/wall.py
@@ -0,0 +1,259 @@
+import math
+
+from compas.geometry import Plane
+from compas.geometry import Point
+from compas.geometry import Polyline
+from compas.geometry import Transformation
+from compas.geometry import angle_vectors
+from compas.geometry import angle_vectors_signed
+from compas.geometry import is_colinear_line_line
+from compas.tolerance import TOL
+from compas_model.elements import Element
+from compas_model.elements import reset_computed
+
+from compas_timber.errors import FeatureApplicationError
+from compas_timber.utils import combine_parallel_segments
+from compas_timber.utils import is_polyline_clockwise
+from compas_timber.utils import join_polyline_segments
+
+from .panel_features import Opening
+from .plate_geometry import PlateGeometry
+from .panel import Panel
+
+
+class PanelType(object):
+    """Constants for different types of panels.
+
+    Attributes
+    ----------
+    WALL : str
+        Constant for wall panels.
+    FLOOR : str
+        Constant for floor panels.
+    ROOF : str
+        Constant for roof panels.
+    GENERIC : str
+        Constant for generic panels.
+    """
+
+    WALL = "wall"
+    FLOOR = "floor"
+    ROOF = "roof"
+    GENERIC = "generic"
+
+
+class Wall(Panel):
+    """Represents a timber panel element (wall, floor, roof, etc.).
+
+    Serves as container for beams, joints and other related elements and groups them together to form a panel.
+    A panel is often a single unit of prefabricated timber element.
+    It is often referred to as an enveloping body.
+
+    Parameters
+    ----------
+    frame : :class:`~compas.geometry.Frame`
+        The coordinate system (frame) of this panel.
+    length : float
+        Length of the panel.
+    width : float
+        Width of the panel.
+    thickness : float
+        Thickness of the panel.
+    outline_a : :class:`~compas.geometry.Polyline`, optional
+        A polyline representing the principal outline of this panel.
+    outline_b : :class:`~compas.geometry.Polyline`, optional
+        A polyline representing the associated outline of this panel.
+    name : str, optional
+        Name of the panel. Defaults to "Panel".
+    **kwargs : dict, optional
+        Additional keyword arguments.
+
+    Attributes
+    ----------
+    frame : :class:`~compas.geometry.Frame`
+        The coordinate system (frame) of this panel.
+    length : float
+        Length of the panel.
+    width : float
+        Width of the panel.
+    height : float
+        Height (thickness) of the panel.
+    thickness : float
+        Thickness of the panel.
+    name : str
+        Name of the panel.
+    interfaces : list
+        List of interfaces associated with this panel.
+    attributes : dict
+        Dictionary of additional attributes.
+    is_panel : bool
+        Always True for panels.
+    is_wall : bool
+        False for base Panel class.
+    is_floor : bool
+        False for base Panel class.
+    is_roof : bool
+        False for base Panel class.
+    is_group_element : bool
+        Always True for panels as they can contain other elements.
+
+    """
+
+    def __repr__(self):
+        return "Wall(name={}, {}, {}, {:.3f})".format(self.name, self.transformation, self.outline_a, self.thickness)
+
+    def __str__(self):
+        return "Wall(name={}, {}, {}, {:.3f})".format(self.name, self.transformation, self.outline_a, self.thickness)
+
+
+
+    @property
+    def is_wall(self):
+        """Check if this element is a wall.
+
+        Returns
+        -------
+        bool
+            False for the base Panel class.
+        """
+        return False
+
+
+
+    @classmethod
+    def from_outlines(cls, outline_a, outline_b, openings=None, design_surface_outside=False, recognize_doors=False, horizontal_openings=False, **kwargs):
+        """
+        Constructs a PlateGeometry from two polyline outlines. to be implemented to instantialte Plates and Panels.
+
+        Parameters
+        ----------
+        outline_a : :class:`~compas.geometry.Polyline`
+            A polyline representing the principal outline of the plate geometry in parent space.
+        outline_b : :class:`~compas.geometry.Polyline`
+            A polyline representing the associated outline of the plate geometry in parent space.
+            This should have the same number of points as outline_a.
+        openings : list[:class:`~compas.geometry.Polyline`], optional
+            A list of openings to be added to the plate geometry.
+        **kwargs : dict, optional
+            Additional keyword arguments to be passed to the constructor.
+
+        Returns
+        -------
+        :class:`~compas_timber.elements.PlateGeometry`
+            A PlateGeometry object representing the plate geometry with the given outlines.
+        """
+        if design_surface_outside:
+            outline_a, outline_b = outline_b, outline_a
+        if openings:
+            openings = [(o, "window") for o in openings]
+        if recognize_doors:
+            outline_a, outline_b, door_openings = extract_door_openings(outline_a, outline_b)
+            if door_openings:
+                if openings is None:
+                    openings = [(o, "door") for o in door_openings]
+                else:
+                    openings.extend([(o, "door") for o in door_openings])
+
+        args = PlateGeometry.get_args_from_outlines(outline_a, outline_b)
+        PlateGeometry._check_outlines(args["local_outline_a"], args["local_outline_b"])
+        kwargs.update(args)
+        kwargs["transformation"] = Transformation.from_frame(args.pop("frame"))
+        panel = cls(**kwargs)
+        if openings:
+            for polyline, opening_type in openings:
+                opening = Opening.from_outline_panel(polyline, panel, opening_type=opening_type, project_horizontal=horizontal_openings)
+                panel.add_feature(opening)
+        return panel
+
+
+def extract_door_openings(outline_a, outline_b):
+    """Extract door openings from the given outlines.
+
+    Parameters
+    ----------
+    outline_a : :class:`~compas.geometry.Polyline`
+        A polyline representing the principal outline of the plate geometry in parent space.
+    outline_b : :class:`~compas.geometry.Polyline`
+        A polyline representing the associated outline of the plate geometry in parent space.
+        This should have the same number of points as outline_a.
+
+    Returns
+    -------
+    list[:class:`~compas.geometry.Polyline`]
+        A list of polylines representing the door openings.
+    """
+    combine_parallel_segments(outline_a)
+    combine_parallel_segments(outline_b)
+    internal_segment_indices_a = get_interior_segment_indices(outline_a)
+    internal_segment_indices_b = get_interior_segment_indices(outline_b)
+    if set(internal_segment_indices_a) != set(internal_segment_indices_b):
+        raise ValueError("The internal segments of outline_a and outline_b do not match.")
+    openings = []
+    done = False
+    while not done:
+        for seg_index in internal_segment_indices_a:
+            i_a = seg_index
+            panel_segments_a = outline_a.lines
+            panel_segments_b = outline_b.lines
+            door_segments = []
+            door_segments_b = []
+            for i in range(i_a - 2, i_a + 3):
+                door_segments.append(panel_segments_a[i % (len(panel_segments_a))])
+                door_segments_b.append(panel_segments_b[i % (len(panel_segments_b))])
+            parallel = True
+            for a, b in zip(door_segments, door_segments_b):
+                if angle_vectors(a.direction, b.direction) > TOL.ABSOLUTE:
+                    parallel = False
+                    break
+            if not parallel:
+                continue
+            side_angle = angle_vectors(door_segments[1].direction, door_segments[3].direction)
+            if abs(side_angle - math.pi) > TOL.ABSOLUTE:
+                continue
+            if not is_colinear_line_line(door_segments[0], door_segments[4], tol=TOL.RELATIVE):
+                continue
+            vertical = door_segments[1].direction
+            vertical.unitize()
+            segs_a = []
+            segs_b = []
+            for i in range(len(panel_segments_a)):
+                if panel_segments_a[i] in door_segments:
+                    continue
+                segs_a.append(panel_segments_a[i])
+                segs_b.append(panel_segments_b[i])
+            opening = join_polyline_segments(door_segments[1:4])
+            opening[0] -= vertical * 1.0
+            opening[3] -= vertical * 1.0
+            opening.append(opening.points[0])  # close loop
+            openings.append(opening)
+            outline_a = join_polyline_segments(segs_a, close_loop=True)
+            outline_b = join_polyline_segments(segs_b, close_loop=True)
+            internal_segment_indices_a = get_interior_segment_indices(outline_a)
+            break
+        else:
+            done = True
+    return outline_a, outline_b, openings
+
+
+def get_interior_corner_indices(polyline):
+    """Get the indices of the interior corners of the panel outline."""
+    _interior_corner_indices = []
+    vector = Plane.from_points(polyline.points).normal
+    points = polyline.points[0:-1]
+    cw = is_polyline_clockwise(polyline, vector)
+    for i in range(len(points)):
+        angle = angle_vectors_signed(points[i - 1] - points[i], points[(i + 1) % len(points)] - points[i], vector, deg=True)
+        if not (cw ^ (angle < 0)):
+            _interior_corner_indices.append(i)
+    return _interior_corner_indices
+
+
+def get_interior_segment_indices(polyline):
+    """Get the indices of the interior segments of the panel outline."""
+    interior_corner_indices = get_interior_corner_indices(polyline)
+    edge_count = len(polyline.points) - 1
+    _interior_segment_indices = []
+    for i in range(edge_count):
+        if i in interior_corner_indices and (i + 1) % edge_count in interior_corner_indices:
+            _interior_segment_indices.append(i)
+    return _interior_segment_indices
