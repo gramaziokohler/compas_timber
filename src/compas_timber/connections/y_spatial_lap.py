d53a63a95f3f853c356bca98fb614f13e57ac969
Gozzi  Eric <ericgozzi@cosmicgarage.ch>
2026-01-22 14:44:00 +0100

    generic lap on cross, remove machining limits

--- src/compas_timber/connections/y_spatial_lap.py
+++ src/compas_timber/connections/y_spatial_lap.py
@@ -175,8 +175,8 @@ class YSpatialLapJoint(Joint):
         self._jack_on_main_beam(self.beam_b)
 
         # Features on cross_beam: Lap cause by beamA and Lap cause by beamB
-        self._lap_a_on_cross()
-        self._lap_b_on_cross()
+        self._cross_lap_from_beam(self.beam_a, self.beam_b)
+        self._cross_lap_from_beam(self.beam_b, self.beam_a)
 
     def _lap_on_main_beam(self, beam, other_beam):
         # Computes the planes
@@ -189,10 +189,8 @@ class YSpatialLapJoint(Joint):
             plane_f = self.cut_plane_a
         elif beam is self.beam_b:
             plane_f = self.cut_plane_b
-
         # Build the polyhedron
         negative_volume = YSpatialLapJoint._volume_from_planes(plane_a, plane_b, plane_c, plane_d, plane_e, plane_f)
-
         # creates and add the processing to the beam
         lap = Lap.from_volume_and_beam(negative_volume, beam, ref_side_index=self.ref_side_index(beam, other_beam))
         beam.add_feature(lap)
@@ -204,58 +202,29 @@ class YSpatialLapJoint(Joint):
             cutting_plane = Plane.from_frame(self.cross_beam.ref_sides[(self.ref_side_index(self.cross_beam, beam) + 2) % 4])
         elif beam is self.beam_b:
             cutting_plane = self.cut_plane_a
+
         # Build the porcessing and add it to the beam
         jack_rafter_cut = JackRafterCut.from_plane_and_beam(cutting_plane, beam)
         beam.add_feature(jack_rafter_cut)
         return cutting_plane
 
-    def _compute_machining_limits(self, beam: Beam) -> MachiningLimits:
-        mac_lim = MachiningLimits()
-        mac_lim.face_limited_front = False
-        mac_lim.face_limited_top = False
+    def _cross_lap_from_beam(self, beam, other_beam) -> Polyhedron:
+        # Computes planes
+        plane_a = Plane.from_frame(beam.ref_sides[(self.ref_side_index(beam, other_beam) + 2) % 4])
+        plane_e = Plane.from_frame(self.cross_beam.ref_sides[self.ref_side_index(self.cross_beam, beam)])
+        plane_d = Plane.from_frame(beam.ref_sides[(self.ref_side_index(beam, self.cross_beam))])
+        plane_b = Plane.from_frame(beam.ref_sides[(self.ref_side_index(beam, self.cross_beam) + 2) % 4])
         if beam is self.beam_a:
-            mac_lim.face_limited_back = False
-        else:
-            mac_lim.face_limited_back = True
-        mac_lim.face_limited_end = True
-        mac_lim.face_limited_bottom = True
-        mac_lim.face_limited_start = False
-        return mac_lim
-
-    def _lap_a_on_cross(self) -> Polyhedron:
-        plane_a = Plane.from_frame(self.beam_a.ref_sides[(self.ref_side_index(self.beam_a, self.beam_b) + 2) % 4])
-        plane_e = Plane.from_frame(self.cross_beam.ref_sides[self.ref_side_index(self.cross_beam, self.beam_a)])
-        plane_c = Plane.from_frame(self.cross_beam.ref_sides[(self.ref_side_index(self.cross_beam, self.beam_a) + 2) % 4])
-        plane_d = Plane.from_frame(self.beam_a.ref_sides[(self.ref_side_index(self.beam_a, self.cross_beam))])
-        plane_b = Plane.from_frame(self.beam_a.ref_sides[(self.ref_side_index(self.beam_a, self.cross_beam) + 2) % 4])
-        plane_f = self.cut_plane_a
-
+            plane_c = Plane.from_frame(self.cross_beam.ref_sides[(self.ref_side_index(self.cross_beam, self.beam_a) + 2) % 4])
+            plane_f = self.cut_plane_a
+        elif beam is self.beam_b:
+            plane_c = self.cut_plane_a
+            plane_f = self.cut_plane_b
+        # Build the Polyhedron
         negative_volume = YSpatialLapJoint._volume_from_planes(plane_a, plane_b, plane_c, plane_d, plane_e, plane_f)
-
-        mac_lim = self._compute_machining_limits(self.beam_a)
-
-        lap_a_on_cross = Lap.from_volume_and_beam(
-            volume=negative_volume, beam=self.cross_beam, ref_side_index=(self.ref_side_index(self.cross_beam, self.beam_b) + 2) % 4, machining_limits=mac_lim.limits
-        )
-        self.cross_beam.add_feature(lap_a_on_cross)
-        return negative_volume
-
-    def _lap_b_on_cross(self) -> Polyhedron:
-        plane_a = Plane.from_frame(self.beam_b.ref_sides[(self.ref_side_index(self.beam_b, self.beam_a) + 2) % 4])
-        plane_e = Plane.from_frame(self.cross_beam.ref_sides[self.ref_side_index(self.cross_beam, self.beam_b)])
-        plane_c = self.cut_plane_a
-        plane_d = Plane.from_frame(self.beam_b.ref_sides[(self.ref_side_index(self.beam_b, self.cross_beam))])
-        plane_b = Plane.from_frame(self.beam_b.ref_sides[(self.ref_side_index(self.beam_b, self.cross_beam) + 2) % 4])
-        plane_f = self.cut_plane_b
-
-        negative_volume = YSpatialLapJoint._volume_from_planes(plane_a, plane_b, plane_c, plane_d, plane_e, plane_f)
-
-        mac_lim = self._compute_machining_limits(self.beam_b)
-
-        lap_b_on_cross = Lap.from_volume_and_beam(
-            volume=negative_volume, beam=self.cross_beam, ref_side_index=(self.ref_side_index(self.cross_beam, self.beam_a) + 2) % 4, machining_limits=mac_lim.limits
-        )
-        self.cross_beam.add_feature(lap_b_on_cross)
+        # Create and add the porcessing
+        lap_on_cross = Lap.from_volume_and_beam(volume=negative_volume, beam=self.cross_beam, ref_side_index=(self.ref_side_index(self.cross_beam, other_beam) + 2) % 4)
+        self.cross_beam.add_feature(lap_on_cross)
         return negative_volume
 
     def restore_beams_from_keys(self, model: TimberModel) -> None:
