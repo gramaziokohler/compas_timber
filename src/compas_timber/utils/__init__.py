170c13c8f68eb1822d39644d34def03715e80045
obucklin <obucklin@gmail.com>
2026-04-28 09:15:30 +0200

    changelog, tests

--- src/compas_timber/utils/__init__.py
+++ src/compas_timber/utils/__init__.py
@@ -25,7 +25,6 @@ from compas.geometry import Transformation
 from compas.geometry import intersection_line_plane
 from compas.geometry import closest_point_on_segment
 from compas.geometry import intersection_segment_segment
-from compas.geometry import intersection_line_segment
 
 from compas.tolerance import TOL
 
@@ -869,69 +868,6 @@ def mesh_from_brep_simple(brep):
     return Mesh.from_vertices_and_faces([v.point for v in brep.vertices], faces_indices)
 
 
-def intersection_line_beams(line, beams, max_distance=None):
-    """Find intersections between a line and a list of beams.
-    Parameters
-    ----------
-    line : :class:`compas.geometry.Line`
-        The line to check for intersections.
-    beams : list of :class:`compas_timber.elements.Beam`
-        The beams to check for intersections.
-    max_distance : float, optional
-        The maximum distance from the line to consider an intersection valid.
-        Defaults to 0.0, meaning no distance check.
-    Returns
-    -------
-    list of dict
-        A list of dictionaries containing the intersection points, dot products, and the corresponding beams.
-    Each dictionary has the keys "point", "dot", and "beam".
-    """
-    intersections = []
-    max_distance = max_distance or TOL.relative
-    for beam in beams:
-        line_pt, beam_pt = intersection_line_segment(line, beam.centerline)
-        if line_pt:
-            if distance_point_point(beam_pt, closest_point_on_segment(beam_pt, beam.centerline)) > max_distance:
-                continue
-            intersection = {}
-            intersection["point"] = Point(*line_pt)
-            intersection["dot"] = dot_vectors(Vector.from_start_end(line.start, Point(*line_pt)), line.direction)
-            intersection["beam"] = beam
-            intersections.append(intersection)
-    return intersections
-
-
-def split_beam_at_lengths(beam, lengths):
-    """Splits a beam at given lengths.
-
-    Parameters
-    ----------
-    beam : :class:`compas_timber.elements.Beam`
-        The beam to split.
-    length : float
-        The length at which to split the beam.
-
-    Returns
-    -------
-    :class:`compas_timber.elements.Beam` or None
-        The new beam that is created by the split, or None if the length is outside the beam's length.
-
-    """
-    lengths.sort(reverse=True)
-    for length in lengths:
-        if length <= 0.0 or length >= beam.length:
-            lengths.remove(length)  # remove lengths that are outside the beam's length
-    beams = [beam]
-    for length in lengths:
-        new_beam = beam.copy()
-        new_beam.attributes.update(beam.attributes)
-        new_beam.length = beam.length - length
-        beam.length = length
-        new_beam.frame.translate(beam.frame.xaxis * length)
-        beams.insert(1, new_beam)
-    return beams
-
-
 def extend_line_segments(segments, close_loop=False):
     """Extend segments to their intersections."""
     start = 0 if close_loop else 1
@@ -943,6 +879,7 @@ def extend_line_segments(segments, close_loop=False):
             continue
         segments[i - 1] = Line(segments[i - 1].start, ints[0])
         segments[i] = Line(ints[0], segments[i].end)
+
 
 def get_interior_corner_indices(polyline):
     """Get the indices of the interior corners of a polyline."""
@@ -966,6 +903,7 @@ def get_interior_segment_indices(polyline):
         if i in interior_corner_indices and (i + 1) % edge_count in interior_corner_indices:
             _interior_segment_indices.append(i)
     return _interior_segment_indices
+
 
 def get_leaf_subclasses(cls):
     subclasses = []
