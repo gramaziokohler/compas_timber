f68fb8705f0fd2ba23bd0c09202139a878366869
nbbosco <nickilu009@gmail.com>
2026-06-05 17:47:11 +0200

    address comments

--- src/compas_timber/connections/i_lap.py
+++ src/compas_timber/connections/i_lap.py
@@ -31,6 +31,8 @@ class ILapJoint(LapJoint):
         If True, the lap is flipped to the other side of the beams.
     cut_plane_bias : float
         Allows lap to be shifted deeper into one beam or the other. Value should be between 0 and 1.0 without completely cutting through either beam. Default is 0.5.
+    lap_length : float, optional
+        The lap length to use. Defaults to the minimum beam height.
 
     Attributes
     ----------
@@ -42,9 +44,21 @@ class ILapJoint(LapJoint):
         If True, the lap is flipped to the other side of the beams.
     cut_plane_bias : float
         Allows lap to be shifted deeper into one beam or the other. Value should be between 0 and 1.0 without completely cutting through either beam. Default is 0.5.
+    lap_length : float or None
+        The lap length to use. When None, defaults to the minimum beam height.
     """
 
     SUPPORTED_TOPOLOGY = JointTopology.TOPO_I
+
+    @property
+    def __data__(self):
+        data = super(ILapJoint, self).__data__
+        data["lap_length"] = self.lap_length
+        return data
+
+    def __init__(self, beam_a=None, beam_b=None, flip_lap_side=False, cut_plane_bias=0.5, lap_length=None, **kwargs):
+        super(ILapJoint, self).__init__(beam_a, beam_b, flip_lap_side, cut_plane_bias, **kwargs)
+        self.lap_length = lap_length
 
     @property
     def ref_side_index_a(self):
@@ -65,17 +79,75 @@ class ILapJoint(LapJoint):
         return self._ref_side_index_b
 
     @staticmethod
-    def _lap_length(beam_a, beam_b):
+    def _end_index_from_side(side):
+        return 4 if side == "start" else 5
+
+    @staticmethod
+    def _endpoint_point(beam, side):
+        return beam.centerline.start if side == "start" else beam.centerline.end
+
+    @staticmethod
+    def _outward_vector(beam, side):
+        return -beam.centerline.direction if side == "start" else beam.centerline.direction
+
+    @classmethod
+    def _select_joint_end_sides(cls, beam_a, beam_b, require_opposite_outward=False):
+        """Return the endpoint-side pair that best represents the beam-beam interface.
+
+        Parameters
+        ----------
+        beam_a : :class:`~compas_timber.elements.Beam`
+        beam_b : :class:`~compas_timber.elements.Beam`
+        require_opposite_outward : bool, optional
+            If True, only side pairs with opposite outward directions are considered.
+
+        Returns
+        -------
+        tuple[str, str]
+            Side labels for beam_a and beam_b, each either ``"start"`` or ``"end"``.
+        """
+        candidates = [("start", "start"), ("start", "end"), ("end", "start"), ("end", "end")]
+        if require_opposite_outward:
+            opposite_candidates = []
+            for side_a, side_b in candidates:
+                out_a = cls._outward_vector(beam_a, side_a)
+                out_b = cls._outward_vector(beam_b, side_b)
+                if out_a.dot(out_b) < 0:
+                    opposite_candidates.append((side_a, side_b))
+            if opposite_candidates:
+                candidates = opposite_candidates
+
+        best_side_pair = None
+        best_distance = None
+        for side_a, side_b in candidates:
+            point_a = cls._endpoint_point(beam_a, side_a)
+            point_b = cls._endpoint_point(beam_b, side_b)
+            distance = point_a.distance_to_point(point_b)
+            if best_distance is None or distance < best_distance:
+                best_distance = distance
+                best_side_pair = (side_a, side_b)
+        return best_side_pair
+
+    def _lap_length(self, beam_a, beam_b):
+        """Resolve the lap length, override when provided by user."""
+        if self.lap_length is not None:
+            max_length = min(beam_a.length, beam_b.length)
+            if self.lap_length > max_length:
+                raise BeamJoiningError("lap_length cannot exceed the minimum beam length.")
+            return self.lap_length
         return min(beam_a.height, beam_b.height)
 
     def _joint_end_sides(self):
+        """Pick the opposing beam ends that define the lap interface."""
         return self._select_joint_end_sides(self.beam_a, self.beam_b, require_opposite_outward=True)
 
     def _joint_end_indices(self):
+        """Map the chosen end sides to reference side indices."""
         side_a, side_b = self._joint_end_sides()
         return self._end_index_from_side(side_a), self._end_index_from_side(side_b)
 
     def _joint_end_state(self):
+        """Compute end points, outward vector, and signed separation."""
         side_a, side_b = self._joint_end_sides()
         point_a = self._endpoint_point(self.beam_a, side_a)
         point_b = self._endpoint_point(self.beam_b, side_b)
@@ -84,6 +156,7 @@ class ILapJoint(LapJoint):
         return side_a, side_b, point_a, point_b, signed_separation
 
     def _assert_is_continuous_i_topology(self):
+        """Validate beams are parallel and colinear for I-topology."""
         angle = angle_vectors(self.beam_a.centerline.direction, self.beam_b.centerline.direction)
         is_parallel = TOL.is_zero(angle) or TOL.is_zero(angle - math.pi)
         if not is_parallel:
@@ -96,6 +169,7 @@ class ILapJoint(LapJoint):
             raise BeamJoiningError("ILapJoint only supports continuous beams in I topology.")
 
     def _cutting_setup(self):
+        """Assemble planes and trim data needed for extension and cuts."""
         side_a, side_b, point_a, point_b, signed_separation = self._joint_end_state()
         end_index_a = self._end_index_from_side(side_a)
         end_index_b = self._end_index_from_side(side_b)
@@ -180,6 +254,13 @@ class ILapJoint(LapJoint):
         depth_a = self.beam_a.height * self.cut_plane_bias
         depth_b = self.beam_b.height * (1.0 - self.cut_plane_bias)
 
+        if trim_each > TOL.absolute:
+            trim_feature_a = JackRafterCutProxy.from_plane_and_beam(cutting_plane_a, self.beam_a)
+            trim_feature_b = JackRafterCutProxy.from_plane_and_beam(cutting_plane_b, self.beam_b)
+        else:
+            trim_feature_a = None
+            trim_feature_b = None
+
         try:
             lap_feature_a = Lap.from_plane_and_beam(cutting_plane_a, self.beam_a, lap_length, depth_a, ref_side_index=self.ref_side_index_a)
             lap_feature_b = Lap.from_plane_and_beam(cutting_plane_b, self.beam_b, lap_length, depth_b, ref_side_index=self.ref_side_index_b)
@@ -189,9 +270,7 @@ class ILapJoint(LapJoint):
         features_a = [lap_feature_a]
         features_b = [lap_feature_b]
 
-        if trim_each > TOL.absolute:
-            trim_feature_a = JackRafterCutProxy.from_plane_and_beam(cutting_plane_a, self.beam_a)
-            trim_feature_b = JackRafterCutProxy.from_plane_and_beam(cutting_plane_b, self.beam_b)
+        if trim_feature_a is not None:
             features_a.insert(0, trim_feature_a)
             features_b.insert(0, trim_feature_b)
 
